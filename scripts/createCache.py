eccb8a431bc7200c90af9eb366db6fcf1bf3240c
gmaillet <gregoire.maillet@gmail.com>
2020-06-10 17:37:31 +0200

    batch create

--- scripts/createCache.py
+++ scripts/createCache.py
@@ -4,6 +4,8 @@ import math
 from pathlib import Path
 import sys
 import numpy as np
+from random import randrange
+import glob
 
 def getCapabilities(input_capabilities):
     tree = ET.parse(input_capabilities)
@@ -55,104 +57,117 @@ def get_TileMatrixSetLimits(tiles, filename):
         tileMatrixSetLimits[tile['Identifier']] = tileMatrixLimits
     return tileMatrixSetLimits
 
+
+def processImage(input_filename, input_r, input_v, input_b):
+    print(input_filename, input_r, input_v, input_b)
+    tileMatixSetLimits = get_TileMatrixSetLimits(tiles, input_filename)
+    input = gdal.Open(input_filename)
+    jpegDriver = gdal.GetDriverByName( 'Jpeg' )
+    pngDriver = gdal.GetDriverByName( 'png' )
+    stem = Path(input_filename).stem
+    # for z in tiles:
+    for z in range(10,18):
+        print('Niveau de zoom : ',z)
+        for x in range(tileMatixSetLimits[z]['MinTileCol'], tileMatixSetLimits[z]['MaxTileCol']):
+            for y in range(tileMatixSetLimits[z]['MinTileRow'], tileMatixSetLimits[z]['MaxTileRow']):
+                # on cree une image 3 canaux pour la tuile
+                opi = create_blank_tile(tiles, z, x, y, 3)
+                # on reech l'OPI dans cette image
+                gdal.Warp(opi, input)
+                # si necessaire on cree le dossier de la tuile
+                dir='cache/'+str(z)+'/'+str(y)+'/'+str(x)
+                Path(dir).mkdir(parents=True, exist_ok=True)
+                # on export en jpeg (todo: gerer le niveau de Q)
+                jpegDriver.CreateCopy( dir+"/"+stem+".jpg", opi)
+                # on cree une image mono canal pour la tuile
+                mask = create_blank_tile(tiles, z, x, y, 1)
+                # on rasterise la partie du graphe qui concerne ce cliche
+                gdal.Rasterize(mask, db, SQLStatement='select geom from graphe_pcrs56_zone_test where cliche = \''+stem+'\' ')
+                img_mask = mask.GetRasterBand(1).ReadAsArray()
+                
+                # si le mask est vide, on a termine
+                max = np.amax(img_mask)
+                if (max>0):
+                    # on cree le graphe et l'ortho
+                    ortho = create_blank_tile(tiles, z, x, y, 3)
+                    graph = create_blank_tile(tiles, z, x, y, 3)
+                    if Path(dir+"/ortho.jpg").is_file():
+                        existing_ortho = gdal.Open(dir+"/ortho.jpg")
+                        existing_graph = gdal.Open(dir+"/graph.png")
+                    else:
+                        existing_ortho = False
+                        existing_graph = False
+
+                    opi_r = opi.GetRasterBand(1).ReadAsArray()
+                    if existing_ortho :
+                        ortho_r = existing_ortho.GetRasterBand(1).ReadAsArray()
+                    else:
+                        ortho_r = ortho.GetRasterBand(1).ReadAsArray()
+                    opi_r[(img_mask == 0)] = 0
+                    ortho_r[(img_mask != 0)] = 0
+                    ortho.GetRasterBand(1).WriteArray(np.add(opi_r, ortho_r))
+
+                    opi_v = opi.GetRasterBand(2).ReadAsArray()
+                    if existing_ortho:
+                        ortho_v = existing_ortho.GetRasterBand(2).ReadAsArray()
+                    else:
+                        ortho_v = ortho.GetRasterBand(2).ReadAsArray()
+                    opi_v[(img_mask == 0)] = 0
+                    ortho_v[(img_mask != 0)] = 0
+                    ortho.GetRasterBand(2).WriteArray(np.add(opi_v, ortho_v))
+
+                    opi_b = opi.GetRasterBand(3).ReadAsArray()
+                    if existing_ortho:
+                        ortho_b = existing_ortho.GetRasterBand(3).ReadAsArray()
+                    else:
+                        ortho_b = ortho.GetRasterBand(3).ReadAsArray()
+                    opi_b[(img_mask == 0)] = 0
+                    ortho_b[(img_mask != 0)] = 0
+                    ortho.GetRasterBand(3).WriteArray(np.add(opi_b, ortho_b))
+
+                    if existing_graph:
+                        graph_r = existing_graph.GetRasterBand(1).ReadAsArray()
+                    else:
+                        graph_r = graph.GetRasterBand(1).ReadAsArray()
+                    graph.GetRasterBand(1).WriteArray(graph_r)
+
+                    if existing_graph:
+                        graph_v = existing_graph.GetRasterBand(2).ReadAsArray()
+                    else:
+                        graph_v = graph.GetRasterBand(2).ReadAsArray()
+                    graph_v[(img_mask != 0)] = input_v
+                    graph.GetRasterBand(2).WriteArray(graph_v)
+
+                    if existing_graph:
+                        graph_b = existing_graph.GetRasterBand(3).ReadAsArray()
+                    else:
+                        graph_b = graph.GetRasterBand(3).ReadAsArray()
+                    graph_b[(img_mask != 0)] = input_b
+                    graph.GetRasterBand(3).WriteArray(graph_b)
+
+                    jpegDriver.CreateCopy( dir+"/ortho.jpg", ortho)
+                    pngDriver.CreateCopy( dir+"/graph.png", graph)
+
+
 tiles, epsg = getCapabilities('Capabilities.xml')
 outRasterSRS = gdal.osr.SpatialReference()
 outRasterSRS.ImportFromEPSG(epsg)
-input_filename = sys.argv[1]
-input_r = int(sys.argv[2])
-input_v = int(sys.argv[3])
-input_b = int(sys.argv[4])
 conn_string = "PG:host=host.docker.internal dbname='pcrs' user='postgres'"
 db = gdal.OpenEx(conn_string, gdal.OF_VECTOR)
 if db is None:
     raise ValueError("Connection to database failed")
-tileMatixSetLimits = get_TileMatrixSetLimits(tiles, input_filename)
-input = gdal.Open(input_filename)
-jpegDriver = gdal.GetDriverByName( 'Jpeg' )
-pngDriver = gdal.GetDriverByName( 'png' )
-stem = Path(input_filename).stem
-for z in tiles:
-# for z in range(17,18):
-    print('Niveau de zoom : ',z)
-    for x in range(tileMatixSetLimits[z]['MinTileCol'], tileMatixSetLimits[z]['MaxTileCol']):
-        for y in range(tileMatixSetLimits[z]['MinTileRow'], tileMatixSetLimits[z]['MaxTileRow']):
-            # on cree une image 3 canaux pour la tuile
-            opi = create_blank_tile(tiles, z, x, y, 3)
-            # on reech l'OPI dans cette image
-            gdal.Warp(opi, input)
-            # si necessaire on cree le dossier de la tuile
-            dir='cache/'+str(z)+'/'+str(y)+'/'+str(x)
-            Path(dir).mkdir(parents=True, exist_ok=True)
-            # on export en jpeg (todo: gerer le niveau de Q)
-            jpegDriver.CreateCopy( dir+"/"+stem+".jpg", opi)
-            # on cree une image mono canal pour la tuile
-            mask = create_blank_tile(tiles, z, x, y, 1)
-            # on rasterise la partie du graphe qui concerne ce cliche
-            gdal.Rasterize(mask, db, SQLStatement='select geom from graphe_pcrs56_zone_test where cliche = \''+stem+'\' ')
-            img_mask = mask.GetRasterBand(1).ReadAsArray()
-            
-            # si le mask est vide, on a termine
-            max = np.amax(img_mask)
-            if (max>0):
-                # on cree le graphe et l'ortho
-                ortho = create_blank_tile(tiles, z, x, y, 3)
-                graph = create_blank_tile(tiles, z, x, y, 3)
-                if Path(dir+"/ortho.jpg").is_file():
-                    existing_ortho = gdal.Open(dir+"/ortho.jpg")
-                    existing_graph = gdal.Open(dir+"/graph.png")
-                else:
-                    existing_ortho = False
-                    existing_graph = False
 
-                opi_r = opi.GetRasterBand(1).ReadAsArray()
-                if existing_ortho :
-                    ortho_r = existing_ortho.GetRasterBand(1).ReadAsArray()
-                else:
-                    ortho_r = ortho.GetRasterBand(1).ReadAsArray()
-                opi_r[(img_mask == 0)] = 0
-                ortho_r[(img_mask != 0)] = 0
-                ortho.GetRasterBand(1).WriteArray(np.add(opi_r, ortho_r))
 
-                opi_v = opi.GetRasterBand(2).ReadAsArray()
-                if existing_ortho:
-                    ortho_v = existing_ortho.GetRasterBand(2).ReadAsArray()
-                else:
-                    ortho_v = ortho.GetRasterBand(2).ReadAsArray()
-                opi_v[(img_mask == 0)] = 0
-                ortho_v[(img_mask != 0)] = 0
-                ortho.GetRasterBand(2).WriteArray(np.add(opi_v, ortho_v))
 
-                opi_b = opi.GetRasterBand(3).ReadAsArray()
-                if existing_ortho:
-                    ortho_b = existing_ortho.GetRasterBand(3).ReadAsArray()
-                else:
-                    ortho_b = ortho.GetRasterBand(3).ReadAsArray()
-                opi_b[(img_mask == 0)] = 0
-                ortho_b[(img_mask != 0)] = 0
-                ortho.GetRasterBand(3).WriteArray(np.add(opi_b, ortho_b))
+L = glob.glob(sys.argv[1])
+# input_r = int(sys.argv[2])
+# input_v = int(sys.argv[3])
+# input_b = int(sys.argv[4])
+for filename in L:
+    processImage(filename, randrange(255), randrange(255), randrange(255))
 
-                if existing_graph:
-                    graph_r = existing_graph.GetRasterBand(1).ReadAsArray()
-                else:
-                    graph_r = graph.GetRasterBand(1).ReadAsArray()
-                graph.GetRasterBand(1).WriteArray(graph_r)
 
-                if existing_graph:
-                    graph_v = existing_graph.GetRasterBand(2).ReadAsArray()
-                else:
-                    graph_v = graph.GetRasterBand(2).ReadAsArray()
-                graph_v[(img_mask != 0)] = input_v
-                graph.GetRasterBand(2).WriteArray(graph_v)
 
-                if existing_graph:
-                    graph_b = existing_graph.GetRasterBand(3).ReadAsArray()
-                else:
-                    graph_b = graph.GetRasterBand(3).ReadAsArray()
-                graph_b[(img_mask != 0)] = input_b
-                graph.GetRasterBand(3).WriteArray(graph_b)
-
-                jpegDriver.CreateCopy( dir+"/ortho.jpg", ortho)
-                pngDriver.CreateCopy( dir+"/graph.png", graph)
 
 
 
