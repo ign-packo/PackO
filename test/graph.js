5c7c004fca693b0e61b530c328f526fb2ace205a
gregoire maillet <gregoire.maillet@ign.fr>
2020-06-30 10:35:37 +0200

    fix test

--- test/graph.js
+++ test/graph.js
@@ -1,9 +1,11 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('..');
+const { json } = require('body-parser');
 
 const should = chai.should();
 chai.use(chaiHttp);
+chai.use(require('chai-json-schema'));
 
 describe('Graph', () => {
   after((done) => {
@@ -15,9 +17,31 @@ describe('Graph', () => {
     it('should return an Id', (done) => {
       chai.request(server)
         .get('/graph')
+        .query({ x: 0, y: 0 })
         .end((err, res) => {
           should.equal(err, null);
           res.should.have.status(200);
+          const json = JSON.parse(res.text);
+          var schema = {
+            title: 'test',
+            type: 'object',
+            required: ['color', 'cliche'],
+            properties: {
+              color: {
+                type: 'array',
+                "minItems": 3,
+                "maxItems": 3,
+                items: {
+                  type: 'integer'
+                }
+              },
+              cliche: {
+                type: 'string'
+              }
+            }
+          };
+          json.should.be.jsonSchema(schema);
+          json.cliche.should.to.equal('unkown');
           done();
         });
     });
