8d7716073767001dfc2cdb4bdfff2674c51141fd
François Toromanoff <francois.toromanoff@ign.fr>
2020-08-27 18:11:44 +0200

    MaJ et modification pour les tests automatiques

--- test/wmts.js
+++ test/wmts.js
@@ -6,56 +6,85 @@ const server = require('..');
 const should = chai.should();
 chai.use(chaiHttp);
 
+const schema = {
+  title: 'test',
+  type: 'object',
+  required: ['color', 'cliche'],
+  properties: {
+    color: {
+      type: 'array',
+      minItems: 3,
+      maxItems: 3,
+      items: {
+        type: 'integer',
+      },
+    },
+    cliche: {
+      type: 'string',
+    },
+  },
+};
+
 describe('Wmts', () => {
   after((done) => {
     server.close();
     done();
   });
 
-  describe('Wrong service', () => {
+  describe('GET /wmts?SERVICE=WRONG&REQUEST=GetCapabilities', () => {
     it('should return an error', (done) => {
       chai.request(server)
         .get('/wmts')
         .query({ REQUEST: 'GetCapabilities', SERVICE: 'WRONG', VERSION: '1.0.0' })
         .end((err, res) => {
-          should.equal(err, null);
+          // should.equal(err, null);
           res.should.have.status(400);
           done();
         });
     });
   });
 
-  describe('Wrong request', () => {
+  describe('GET /wmts?SERVICE=WMTS&REQUEST=Wrong', () => {
     it('should return an error', (done) => {
       chai.request(server)
         .get('/wmts')
-        .query({ REQUEST: 'WRONG', SERVICE: 'WMTS', VERSION: '1.0.0' })
+        .query({ REQUEST: 'Wrong', SERVICE: 'WMTS', VERSION: '1.0.0' })
         .end((err, res) => {
-          should.equal(err, null);
+          // should.equal(err, null);
           res.should.have.status(400);
           done();
         });
     });
   });
 
-  describe('Get Capabilities', () => {
-    it('should return an xml', (done) => {
-      // setTimeout(done, 10000);
+  describe('GetCapabilities', () => {
+    it('should return the Capabilities.xml', (done) => {
       chai.request(server)
         .get('/wmts')
         .query({ REQUEST: 'GetCapabilities', SERVICE: 'WMTS', VERSION: '1.0.0' })
         .end((err, res) => {
-          should.equal(err, null);
           res.should.have.status(200);
-          // res.body.should.be.an('array');
+          res.type.should.be.a('string').equal('application/xml');
+
           done();
         });
-    }).timeout(20000);
+    });
   });
 
-  describe('Get Tile', () => {
-    it('should return an image', (done) => {
-      // setTimeout(done, 10000);
+  describe('GetTile', () => {
+    it('should return an error', (done) => {
+      chai.request(server)
+        .get('/wmts')
+        .query({
+          REQUEST: 'GetTile', SERVICE: 'WMTS', VERSION: '1.0.0', TILEMATRIX: 12, TILEROW: 0, TILECOL: 0, FORMAT: 'image/autre', LAYER: 'ortho',
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should return an image png', (done) => {
       chai.request(server)
         .get('/wmts')
         .query({
@@ -64,43 +93,42 @@ describe('Wmts', () => {
         .end((err, res) => {
           should.equal(err, null);
           res.should.have.status(200);
-          // res.body.should.be.an('array');
+          res.type.should.be.a('string').equal('application/octet-stream');
+
+          done();
+        });
+    });
+
+    it('should return an image jpeg', (done) => {
+      chai.request(server)
+        .get('/wmts')
+        .query({
+          REQUEST: 'GetTile', SERVICE: 'WMTS', VERSION: '1.0.0', TILEMATRIX: 12, TILEROW: 0, TILECOL: 0, FORMAT: 'image/jpeg', LAYER: 'ortho',
+        })
+        .end((err, res) => {
+          should.equal(err, null);
+          res.should.have.status(200);
+          res.type.should.be.a('string').equal('application/octet-stream');
+
           done();
         });
     });
   });
 
-  describe('Get FeatureInfo', () => {
-    it('should return a valid json', (done) => {
+  describe('GetFeatureInfo', () => {
+    it("should return a Json contening 'color' and 'cliche' (different de 'unknown')", (done) => {
       chai.request(server)
         .get('/wmts')
         .query({
-          REQUEST: 'GetFeatureInfo', SERVICE: 'WMTS', VERSION: '1.0.0', TILEMATRIX: 12, TILEROW: 0, TILECOL: 0, I: 0, J: 0,
+          REQUEST: 'GetFeatureInfo', SERVICE: 'WMTS', VERSION: '1.0.0', TILEMATRIX: 21, TILEROW: 409395, TILECOL: 18027, I: 10, J: 10,
         })
         .end((err, res) => {
-          should.equal(err, null);
+          // should.equal(err, null);
           res.should.have.status(200);
-          const json = JSON.parse(res.text);
-          const schema = {
-            title: 'test',
-            type: 'object',
-            required: ['color', 'cliche'],
-            properties: {
-              color: {
-                type: 'array',
-                minItems: 3,
-                maxItems: 3,
-                items: {
-                  type: 'integer',
-                },
-              },
-              cliche: {
-                type: 'string',
-              },
-            },
-          };
-          json.should.be.jsonSchema(schema);
-          json.cliche.should.to.equal('unknown');
+          const resJson = JSON.parse(res.text);
+
+          resJson.should.be.jsonSchema(schema);
+          resJson.should.have.property('cliche').not.equal('unknown');
           done();
         });
     });
