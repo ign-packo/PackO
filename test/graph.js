2e02acc81bcd4794116234e7e47130829d4c5481
François Toromanoff <francois.toromanoff@ign.fr>
2020-08-27 18:11:44 +0200

    MaJ et modification pour les tests automatiques

--- test/graph.js
+++ test/graph.js
@@ -3,9 +3,28 @@ const chaiHttp = require('chai-http');
 // const { json } = require('body-parser');
 const server = require('..');
 
-const should = chai.should();
+// const should = chai.should();
 chai.use(chaiHttp);
 chai.use(require('chai-json-schema'));
+
+const schema = {
+  title: 'test',
+  type: 'object',
+  required: ['color', 'cliche'],
+  properties: {
+    color: {
+      type: 'array',
+      minItems: 3,
+      maxItems: 3,
+      items: {
+        type: 'integer',
+      },
+    },
+    cliche: {
+      type: 'string',
+    },
+  },
+};
 
 describe('Graph', () => {
   after((done) => {
@@ -13,71 +32,78 @@ describe('Graph', () => {
     done();
   });
 
-  describe('Get graph', () => {
-    it("should return a Json contening 'color' and 'cliche'", (done) => {
-      chai.request(server)
-        .get('/graph')
-        .query({ x: 230975.8, y: 6759518.1 })
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(200);
-          const json = JSON.parse(res.text);
-          const schema = {
-            title: 'test',
-            type: 'object',
-            required: ['color', 'cliche'],
-            properties: {
-              color: {
-                type: 'array',
-                minItems: 3,
-                maxItems: 3,
-                items: {
-                  type: 'integer',
-                },
-              },
-              cliche: {
-                type: 'string',
-              },
-            },
-          };
-          json.should.be.jsonSchema(schema);
-          // json.cliche.should.to.equal('unknown');
-          json.should.have.cliche('unknown');
-          done();
-        });
+  describe('GET /graph', () => {
+    describe('query : x=0 & y=0', () => {
+      it("should return an 'unknown' cliche", (done) => {
+        chai.request(server)
+          .get('/graph')
+          .query({ x: 0, y: 0 })
+          .end((err, res) => {
+            // should.equal(err, null);
+            res.should.have.status(200);
+            const resJson = JSON.parse(res.text);
+
+            resJson.should.be.jsonSchema(schema);
+            resJson.should.have.property('cliche').equal('unknown');
+
+            done();
+          });
+      });
+    });
+    describe('query : x=230752.8 & y=6759737.1', () => {
+      it("should return a Json contening 'color' and 'cliche' (different de 'unknown')", (done) => {
+        chai.request(server)
+          .get('/graph')
+          .query({ x: 230752.8, y: 6759737.1 })
+          .end((err, res) => {
+            // should.equal(err, null);
+            res.should.have.status(200);
+            const resJson = JSON.parse(res.text);
+            resJson.should.be.jsonSchema(schema);
+            resJson.should.have.property('cliche').not.equal('unknown');
+
+            done();
+          });
+      });
     });
   });
 
-  describe('Post graph/patch', () => {
-    it('should return an error', (done) => {
-      chai.request(server)
-        .post('/graph/patch')
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(400);
-          done();
-        });
+  describe('POST /graph/patch', () => {
+    describe('body : {}', () => {
+      it('should return an error', (done) => {
+        chai.request(server)
+          .post('/graph/patch')
+          .end((err, res) => {
+            // should.equal(err, null);
+            res.should.have.status(400);
+            done();
+          });
+      });
     });
-  });
 
-  describe('Post graph/patch', () => {
-    it('should return an works', (done) => {
-      chai.request(server)
-        .post('/graph/patch')
-        .send({
-          type: 'FeatureCollection',
-          features: [
-            {
-              type: 'Feature',
-              properties: { color: [0, 0, 0], cliche: 'unkown' },
-              geometry: { type: 'Polygon', coordinates: [[[0, 0], [10, 0], [10, 10], [0, 10], [0, 0]]] },
-            }],
-        })
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(400);
-          done();
-        });
+    describe('body : polygon geoJson', () => {
+      it('should apply the patch and return the liste of tiles impacted', (done) => {
+        chai.request(server)
+          .post('/graph/patch')
+          .send({
+            type: 'FeatureCollection',
+            crs: { type: 'name', properties: { name: 'urn:ogc:def:crs:EPSG::2154' } },
+            features: [
+              {
+                type: 'Feature',
+                properties: { color: [99, 167, 133], cliche: '19FD5606Ax00020_16371' },
+                geometry: { type: 'Polygon', coordinates: [[[230748, 6759736], [230746, 6759736], [230746, 6759734], [230748, 6759734], [230748, 6759736]]] },
+              }],
+          })
+          .end((err, res) => {
+            // should.equal(err, null);
+            res.should.have.status(200);
+            const resJson = JSON.parse(res.text);
+            resJson.should.be.a('array');
+
+            done();
+          });
+      });
     });
   });
 });
---
+++ test/validator.js
@@ -0,0 +1,71 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const validator = require('../paramValidation/validator');
+// const isCrs = require('../paramValidation/validator/isCrs');
+
+chai.use(chaiHttp);
+
+describe('Validator', () => {
+  describe('isCrs', () => {
+    describe("without the key 'type'", () => {
+      it("should return 'false'", (done) => {
+        const crs = JSON.parse('{"properties": { "name": "urn:ogc:def:crs:EPSG::2154" } }');
+        validator.isCrs(crs).should.be.a('boolean').equal(false);
+        done();
+      });
+    });
+    describe('type : name', () => {
+      it("should return 'true'", (done) => {
+        const crs = JSON.parse('{"type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::2154" } }');
+        validator.isCrs(crs).should.be.a('boolean').equal(true);
+        done();
+      });
+    });
+    describe('type : EPSG', () => {
+      it("should return 'true'", (done) => {
+        const crs = JSON.parse('{"type": "EPSG", "properties": { "code": "2154" } }');
+        validator.isCrs(crs).should.be.a('boolean').equal(true);
+        done();
+      });
+    });
+    describe('type : other', () => {
+      it("should return 'false'", (done) => {
+        const crs = JSON.parse('{"type": "other", "properties": { "other": "2154" } }');
+        validator.isCrs(crs).should.be.a('boolean').equal(false);
+        done();
+      });
+    });
+  });
+
+  describe('isColor', () => {
+    describe("color='[234, 125, 589]' (wrong format)", () => {
+      it("should return 'false'", (done) => {
+        const color = '[234, 125, 589]';
+        validator.isColor(color).should.be.a('boolean').equal(false);
+        done();
+      });
+    });
+    describe('color : [234, 125, 589] (value > 255)', () => {
+      it("should return 'false'", (done) => {
+        const color = [234, 125, 589];
+        validator.isColor(color).should.be.a('boolean').equal(false);
+        done();
+      });
+    });
+    describe('color : [234, 125] (missing a value)', () => {
+      it("should return 'false'", (done) => {
+        const color = [234, 125];
+        validator.isColor(color).should.be.a('boolean').equal(false);
+        done();
+      });
+    });
+    describe("color : ['234', 0, 185]", () => {
+      it("should return 'true'", (done) => {
+        const color = ['234', 0, 185];
+        validator.isColor(color).should.be.a('boolean').equal(true);
+        done();
+      });
+    });
+  });
+});
