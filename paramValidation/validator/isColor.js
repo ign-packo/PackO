917bba4e0734fc010b692b780aac8aeefc916ebe
François Toromanoff <francois.toromanoff@ign.fr>
2020-08-27 18:09:21 +0200

    [isColor] Fix test length

--- paramValidation/validator/isColor.js
+++ paramValidation/validator/isColor.js
@@ -4,7 +4,7 @@ function isInt(value) {
 }
 module.exports = function isColor(color) {
   if (!Array.isArray(color)) return false;
-  if (!color.length === 3) return false;
+  if (color.length !== 3) return false;
   for (let i = 0; i < color.length; i += 1) {
     if (!(isInt(color[i]) && color[i] >= 0 && color[i] < 256)) return false;
   }
