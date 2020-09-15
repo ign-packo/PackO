eda466259abe8b87fbbfd5c4def80b770f1f8c5b
François Toromanoff <francois.toromanoff@ign.fr>
2020-09-15 11:05:44 +0200

    Fix createErrMsg.js

--- paramValidation/createErrMsg.js
+++ paramValidation/createErrMsg.js
@@ -2,7 +2,6 @@ module.exports = {
   missingParameter(param) { return `Le paramètre '${param}' est requis.`; },
   invalidParameter(param) { return `Le paramètre '${param}' n'est pas valide.`; },
   InvalidEntite(param, entite) { return `Le parametre '${param}' n'est pas un ${entite} valide.`; },
-  incompletParameter(param) { return `Le paramètre '${param}' est incomplet.`; },
 
   missingBody() { return 'Un body non vide est requis.'; },
   invalidBody(param) { return `le body n'est pas un '${param}' valide.`; },
