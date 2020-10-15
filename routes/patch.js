127e4249043418b16fe6b286ba8d2c2b20898266
François Toromanoff <francois.toromanoff@ign.fr>
2020-10-15 16:26:46 +0200

    [test/patch] Mise a jout des tests pour les patchs

--- routes/patch.js
+++ routes/patch.js
@@ -95,6 +95,11 @@ function getTiles(features, overviews) {
   return tiles;
 }
 
+router.get('/patchs', [], (req, res) => {
+  debug('~~~GET patchs');
+  res.status(200).send(JSON.stringify(req.app.activePatchs));
+});
+
 router.post('/patch', encapBody.bind({ keyName: 'geoJSON' }), [
   ...geoJsonAPatcher,
 ], validateParams, (req, res) => {
@@ -121,140 +126,157 @@ router.post('/patch', encapBody.bind({ keyName: 'geoJSON' }), [
   newPatchId += 1;
 
   const tiles = getTiles(geoJson.features, overviews);
-  debug(tiles.length, 'tuiles impactées');
-  // Patch these tiles
-  const errors = [];
-  tiles.forEach((tile) => {
-    // Patch du graph
-    debug(tile);
+  debug(tiles.length, 'tuiles intersectées');
+  const tilesModified = [];
 
-    const resolution = Rmax * 2 ** (lvlMax - tile.z);
-    const urlGraph = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'graph.png');
-    const urlOrtho = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'ortho.png');
-
-    const urlGraphOutput = path.join(global.dir_cache, tile.z, tile.y, tile.x, `graph_${newPatchId}.png`);
-    const urlOrthoOutput = path.join(global.dir_cache, tile.z, tile.y, tile.x, `ortho_${newPatchId}.png`);
-
-    const urlOpi = path.join(global.dir_cache, tile.z, tile.y, tile.x, `${geoJson.features[0].properties.cliche}.png`);
-    if (!fs.existsSync(urlGraph) || !fs.existsSync(urlOrtho) || !fs.existsSync(urlOpi)) {
-      errors.push('file not exists');
-      debug('ERROR');
-      return;
-    }
-    const mask = PImage.make(tileWidth, tileHeight);
-    const ctx = mask.getContext('2d');
-    geoJson.features.forEach((feature) => {
-      // debug(feature.properties.color);
-      ctx.fillStyle = '#FFFFFF';
-      ctx.beginPath();
-      let first = true;
-      /* eslint-disable no-restricted-syntax */
-      for (const point of feature.geometry.coordinates[0]) {
-        const i = Math.round((point[0] - xOrigin - tile.x * tileWidth * resolution)
-            / resolution);
-        const j = Math.round((yOrigin - point[1] - tile.y * tileHeight * resolution)
-            / resolution);
-        if (first) {
-          first = false;
-          ctx.moveTo(i, j);
-        } else {
-          ctx.lineTo(i, j);
-        }
-      }
-      ctx.closePath();
-      ctx.fill();
-    });
-
-    // On patch le graph
-    /* eslint-disable no-param-reassign */
-    promises.push(jimp.read(urlGraph).then((graph) => {
-      for (let idx = 0; idx < 256 * 256 * 4; idx += 4) {
-        if (mask.data[idx + 3]) {
-          [graph.bitmap.data[idx],
-            graph.bitmap.data[idx + 1],
-            graph.bitmap.data[idx + 2]] = geoJson.features[0].properties.color;
-        }
-      }
-      return graph.writeAsync(urlGraphOutput);
-    }).then(() => {
-      debug('graph done');
-    }));
-
-    // On patch l ortho
-    /* eslint-disable no-param-reassign */
-    const promiseOrthoOpi = [jimp.read(urlOrtho), jimp.read(urlOpi)];
-    promises.push(Promise.all(promiseOrthoOpi).then((images) => {
-      const ortho = images[0];
-      const opi = images[1];
-      for (let idx = 0; idx < 256 * 256 * 4; idx += 4) {
-        if (mask.data[idx + 3]) {
-          ortho.bitmap.data[idx] = opi.bitmap.data[idx];
-          ortho.bitmap.data[idx + 1] = opi.bitmap.data[idx + 1];
-          ortho.bitmap.data[idx + 2] = opi.bitmap.data[idx + 2];
-        }
-      }
-      return ortho.writeAsync(urlOrthoOutput);
-    }).then(() => {
-      debug('ortho done');
-    }));
-  });
-  if (errors.length) {
-    res.status(500).send(errors);
-  }
-  Promise.all(promises).then(() => {
-    debug('tout c est bien passé on peut mettre a jour les liens symboliques');
+  try {
+    // Patch these tiles
+    const outOfBoundsTiles = [];
     tiles.forEach((tile) => {
+      // Patch du graph
+      debug(tile);
+
       const urlGraph = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'graph.png');
       const urlOrtho = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'ortho.png');
+      const urlOpi = path.join(global.dir_cache, tile.z, tile.y, tile.x, `${geoJson.features[0].properties.cliche}.png`);
+
+      if (!fs.existsSync(urlGraph) || !fs.existsSync(urlOrtho) || !fs.existsSync(urlOpi)) {
+        outOfBoundsTiles.push(`${global.dir_cache}/${tile.z}/${tile.y}/${tile.x}`);
+        debug('Out of bonds');
+        return;
+      }
+
       const urlGraphOutput = path.join(global.dir_cache, tile.z, tile.y, tile.x, `graph_${newPatchId}.png`);
       const urlOrthoOutput = path.join(global.dir_cache, tile.z, tile.y, tile.x, `ortho_${newPatchId}.png`);
-      // on verifie si c'est un lien symbolique ou le fichier d'origine
-      // if (fs.lstatSync(urlGraph).isSymbolicLink()) {
-      if (fs.lstatSync(urlGraph).nlink > 1) {// ou faire test sur la presence d'un fichier _orig ??
-        fs.unlinkSync(urlGraph);
-        fs.unlinkSync(urlOrtho);
+
+      const mask = PImage.make(tileWidth, tileHeight);
+      const ctx = mask.getContext('2d');
+      geoJson.features.forEach((feature) => {
+        // debug(feature.properties.color);
+        ctx.fillStyle = '#FFFFFF';
+        ctx.beginPath();
+        let first = true;
+        /* eslint-disable no-restricted-syntax */
+        const resolution = Rmax * 2 ** (lvlMax - tile.z);
+        for (const point of feature.geometry.coordinates[0]) {
+          const i = Math.round((point[0] - xOrigin - tile.x * tileWidth * resolution)
+                / resolution);
+          const j = Math.round((yOrigin - point[1] - tile.y * tileHeight * resolution)
+                / resolution);
+          if (first) {
+            first = false;
+            ctx.moveTo(i, j);
+          } else {
+            ctx.lineTo(i, j);
+          }
+        }
+        ctx.closePath();
+        ctx.fill();
+      });
+
+      // On patch le graph
+      /* eslint-disable no-param-reassign */
+      promises.push(jimp.read(urlGraph).then((graph) => {
+        for (let idx = 0; idx < 256 * 256 * 4; idx += 4) {
+          if (mask.data[idx + 3]) {
+            [graph.bitmap.data[idx],
+              graph.bitmap.data[idx + 1],
+              graph.bitmap.data[idx + 2]] = geoJson.features[0].properties.color;
+          }
+        }
+        return graph.writeAsync(urlGraphOutput);
+      }).then(() => {
+        debug('graph done');
+      }));
+
+      // On patch l ortho
+      /* eslint-disable no-param-reassign */
+      const promiseOrthoOpi = [jimp.read(urlOrtho), jimp.read(urlOpi)];
+      promises.push(Promise.all(promiseOrthoOpi).then((images) => {
+        const ortho = images[0];
+        const opi = images[1];
+        for (let idx = 0; idx < 256 * 256 * 4; idx += 4) {
+          if (mask.data[idx + 3]) {
+            ortho.bitmap.data[idx] = opi.bitmap.data[idx];
+            ortho.bitmap.data[idx + 1] = opi.bitmap.data[idx + 1];
+            ortho.bitmap.data[idx + 2] = opi.bitmap.data[idx + 2];
+          }
+        }
+        return ortho.writeAsync(urlOrthoOutput);
+      }).then(() => {
+        debug('ortho done');
+      }));
+      tilesModified.push(tile);
+    });
+    if (outOfBoundsTiles.length) {
+      const err = new Error();
+      err.msg = {
+        status: 'File(s) missing',
+        errors: [{
+          localisation: outOfBoundsTiles,
+          value: `${geoJson.features[0].properties.cliche}.png ou graph.png ou ortho.png`,
+          msg: 'File(s) missing',
+        }],
+      };
+      throw err;
+    }
+    Promise.all(promises).then(() => {
+      debug('tout c est bien passé on peut mettre a jour les liens symboliques');
+      tilesModified.forEach((tile) => {
+        const urlGraph = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'graph.png');
+        const urlOrtho = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'ortho.png');
+        const urlGraphOutput = path.join(global.dir_cache, tile.z, tile.y, tile.x, `graph_${newPatchId}.png`);
+        const urlOrthoOutput = path.join(global.dir_cache, tile.z, tile.y, tile.x, `ortho_${newPatchId}.png`);
+        // on verifie si c'est un lien symbolique ou le fichier d'origine
+        // if (fs.lstatSync(urlGraph).isSymbolicLink()) {
+        if (fs.lstatSync(urlGraph).nlink > 1) {
+          // ou faire test sur la presence d'un fichier _orig ??
+          fs.unlinkSync(urlGraph);
+          fs.unlinkSync(urlOrtho);
+        } else {
+          const urlGraphOrig = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'graph_orig.png');
+          const urlOrthoOrig = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'ortho_orig.png');
+          fs.renameSync(urlGraph, urlGraphOrig);
+          fs.renameSync(urlOrtho, urlOrthoOrig);
+        }
+        // fs.symlinkSync(urlGraphOutput, urlGraph);
+        // fs.symlinkSync(urlOrthoOutput, urlOrtho);
+        fs.linkSync(urlGraphOutput, urlGraph);
+        fs.linkSync(urlOrthoOutput, urlOrtho);
+      });
+      // on note le patch Id
+      geoJson.features.forEach((feature) => {
+        feature.properties.patchId = newPatchId;
+      });
+      // on ajoute ce patch à l'historique
+      debug('New patch, Id:', newPatchId);
+      req.app.activePatchs.features = req.app.activePatchs.features.concat(geoJson.features);
+      debug('features in activePatchs:', req.app.activePatchs.features.length);
+
+      // on sauve l'historique (au cas ou l'API devrait etre relancee)
+      fs.writeFileSync(path.join(global.dir_cache, 'activePatchs.json'), JSON.stringify(req.app.activePatchs, null, 4));
+
+      // on purge les patchs inactifs puisqu'on ne pourra plus les appliquer
+      req.app.unactivePatchs.features = [];
+      debug('features in unactivePatchs:', req.app.unactivePatchs.features.length);
+      fs.writeFileSync(path.join(global.dir_cache, 'unactivePatchs.json'), JSON.stringify(req.app.unactivePatchs, null, 4));
+      if (outOfBoundsTiles.length) {
+        res.status(204).send(JSON.stringify(tiles));
       } else {
-        const urlGraphOrig = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'graph_orig.png');
-        const urlOrthoOrig = path.join(global.dir_cache, tile.z, tile.y, tile.x, 'ortho_orig.png');
-        fs.renameSync(urlGraph, urlGraphOrig);
-        fs.renameSync(urlOrtho, urlOrthoOrig);
+        res.status(200).send(JSON.stringify(tiles));
       }
-      // fs.symlinkSync(urlGraphOutput, urlGraph);
-      // fs.symlinkSync(urlOrthoOutput, urlOrtho);
-      fs.linkSync(urlGraphOutput, urlGraph);
-      fs.linkSync(urlOrthoOutput, urlOrtho);
+    }).catch((err) => {
+      debug('erreur : ', err);
+      // todo: il faut tout annuler
+      res.status(500).send(err);
     });
-    // on note le patch Id
-    geoJson.features.forEach((feature) => {
-      feature.properties.patchId = newPatchId;
-    });
-    // on ajoute ce patch à l'historique
-    debug('New patch, Id:', newPatchId);
-    req.app.activePatchs.features = req.app.activePatchs.features.concat(geoJson.features);
-    debug('features in activePatchs:', req.app.activePatchs.features.length);
-
-    // on sauve l'historique (au cas ou l'API devrait etre relancee)
-    fs.writeFileSync(path.join(global.dir_cache, 'activePatchs.json'), JSON.stringify(req.app.activePatchs, null, 4));
-
-    // on purge les patchs inactifs puisqu'on ne pourra plus les appliquer
-    req.app.unactivePatchs.features = [];
-    debug('features in unactivePatchs:', req.app.unactivePatchs.features.length);
-    fs.writeFileSync(path.join(global.dir_cache, 'unactivePatchs.json'), JSON.stringify(req.app.unactivePatchs, null, 4));
-    res.status(200).send(JSON.stringify(tiles));
-  }).catch((err) => {
-    debug('erreur : ', err);
-    // todo: il faut tout annuler
+  } catch (err) {
     res.status(500).send(err);
-  });
+  }
 });
 
-router.get('/patchs', [], (req, res) => {
-  debug('~~~GET patchs');
-  res.status(200).send(JSON.stringify(req.app.activePatchs));
-});
-
-router.put('/patchs/undo', [], (req, res) => {
-  debug('~~~PUT patchs/undo');
+router.put('/patch/undo', [], (req, res) => {
+  debug('~~~PUT patch/undo');
   const { overviews } = req.app;
   if (req.app.activePatchs.features.length === 0) {
     debug('nothing to undo');
@@ -317,8 +339,8 @@ router.put('/patchs/undo', [], (req, res) => {
   res.status(200).send(`undo ${lastPatchId} succeed`);
 });
 
-router.put('/patchs/redo', [], (req, res) => {
-  debug('~~~PUT patchs/redo');
+router.put('/patch/redo', [], (req, res) => {
+  debug('~~~PUT patch/redo');
   const { overviews } = req.app;
   if (req.app.unactivePatchs.features.length === 0) {
     debug('nothing to redo');
--- test/patch.js
+++ test/patch.js
@@ -1,13 +1,12 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-// const { json } = require('body-parser');
-const server = require('..');
-
-const should = chai.should();
-chai.use(chaiHttp);
+chai.use(require('chai-http'));
 chai.use(require('chai-json-schema'));
 
-describe('Patchs', () => {
+const should = chai.should();
+const GJV = require('geojson-validation');
+const server = require('..');
+
+describe('Patch', () => {
   after((done) => {
     server.close();
     done();
@@ -25,7 +24,6 @@ describe('Patchs', () => {
           });
       });
     });
-
     describe('body: polygon geoJson', () => {
       it('should apply the patch and return the liste of tiles impacted', (done) => {
         chai.request(server)
@@ -49,8 +47,8 @@ describe('Patchs', () => {
             done();
           });
       });
-
-      it('should get an error: missing data', (done) => {
+      // TODO gestion des polygones Out of bounds
+      it("should get an error: 'File(s) missing", (done) => {
         chai.request(server)
           .post('/patch')
           .send({
@@ -66,61 +64,94 @@ describe('Patchs', () => {
           .end((err, res) => {
             should.not.exist(err);
             res.should.have.status(500);
-
             done();
           });
       });
     });
   });
 
-  describe('Put undo', () => {
-    it('should succeed', (done) => {
-      chai.request(server)
-        .put('/patchs/undo')
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-
-  describe('Put redo', () => {
-    it('should succeed', (done) => {
-      chai.request(server)
-        .put('/patchs/redo')
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-
-  describe('Get', () => {
+  describe('GET /patchs', () => {
     it('should return an valid geoJson', (done) => {
       chai.request(server)
         .get('/patchs')
         .end((err, res) => {
-          should.equal(err, null);
+          should.not.exist(err);
           res.should.have.status(200);
+          const resJson = JSON.parse(res.text);
+          GJV.isGeoJSONObject(resJson).should.be.a('boolean').equal(true);
+          GJV.isFeatureCollection(resJson).should.be.a('boolean').equal(true);
           done();
         });
     });
   });
 
-  describe('Put clear', () => {
+  describe('PUT /patch/undo', () => {
+    it('should succeed', (done) => {
+      chai.request(server)
+        .put('/patch/undo')
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(200);
+          res.text.should.equal('undo 1 succeed');
+          done();
+        });
+    });
+    it("should return a warning (code 201): 'nothing to undo'", (done) => {
+      chai.request(server)
+        .put('/patch/undo')
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(201);
+          res.text.should.equal('nothing to undo');
+          done();
+        });
+    });
+  });
+  describe('PUT /patch/redo', () => {
+    it('should succeed', (done) => {
+      chai.request(server)
+        .put('/patch/redo')
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(200);
+          res.text.should.equal('redo 1 succeed');
+          done();
+        });
+    });
+    it("should return a warning (code 201): 'nothing to redo'", (done) => {
+      chai.request(server)
+        .put('/patch/redo')
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(201);
+          res.text.should.equal('nothing to redo');
+          done();
+        });
+    });
+  });
+  describe('PUT /patchs/clear', () => {
     it('should succeed', (done) => {
       chai.request(server)
         .put('/patchs/clear')
         .end((err, res) => {
-          should.equal(err, null);
+          should.not.exist(err);
           res.should.have.status(200);
+          res.text.should.equal('clear succeed');
+          done();
+        });
+    });
+    it("should return a warning (code 201): 'nothing to clear'", (done) => {
+      chai.request(server)
+        .put('/patchs/clear')
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(201);
+          res.text.should.equal('nothing to clear');
           done();
         });
     });
   });
-
+  /*
   describe('Put undo', () => {
     it('should return an error (nothing to undo)', (done) => {
       chai.request(server)
@@ -132,28 +163,5 @@ describe('Patchs', () => {
         });
     });
   });
-
-  describe('Put redo', () => {
-    it('should return an error (nothing to redo)', (done) => {
-      chai.request(server)
-        .put('/patchs/redo')
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(500);
-          done();
-        });
-    });
-  });
-
-  describe('Put clear', () => {
-    it('should return an error (nothing to clear)', (done) => {
-      chai.request(server)
-        .put('/patchs/clear')
-        .end((err, res) => {
-          should.equal(err, null);
-          res.should.have.status(500);
-          done();
-        });
-    });
-  });
+  */
 });
