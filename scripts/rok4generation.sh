c2d078619725370804d85e96862a02ecd24068fb
greg maillet <gregoire.maillet@ign.fr>
2020-11-13 14:13:49 +0100

    gestion du georef

--- scripts/rok4generation.sh
+++ scripts/rok4generation.sh
@@ -1,8 +1,7 @@
-#docker run -ti --rm -v `pwd`/cache:/cache -v `pwd`/scripts:/scripts rok4/rok4generation:3.7.2-buster bash
-
 for nom in $@
 do
 out=`echo $nom | perl -pe 's/png/tif/g'`
 echo $out
 work2cache $nom -c lzw $out
+cp $nom.aux.xml $out.aux.xml
 done
