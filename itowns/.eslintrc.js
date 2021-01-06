3b67cf373b3fee147896c823db83a4c1d7b4cce9
François Toromanoff <francois.toromanoff@ign.fr>
2021-01-06 10:51:44 +0100

    [itowns/eslint] config

---
+++ itowns/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
