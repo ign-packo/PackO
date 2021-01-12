ea1792ca6ea5362b3eb1fba603d35c0b4f9e850e
greg maillet <gregoire.maillet@ign.fr>
2021-01-12 16:51:44 +0100

    feat(cache): slab instead of tile

--- rok4.js
+++ rok4.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const debug = require('debug')('rok4');
 
-function getPath(X, Y, Z, pathDepth) {
+function getPath(X, Y, Z, pathDepth, slabSize) {
   debug('~~~getPath');
   debug(X, Y, Z, pathDepth);
-  const strX = Math.trunc(X).toString(36).padStart(pathDepth + 1, 0).toUpperCase();
-  const strY = Math.trunc(Y).toString(36).padStart(pathDepth + 1, 0).toUpperCase();
+  const strX = Math.trunc(X / slabSize.width)
+    .toString(36).padStart(pathDepth + 1, 0).toUpperCase();
+  const strY = Math.trunc(Y / slabSize.height)
+    .toString(36).padStart(pathDepth + 1, 0).toUpperCase();
   debug(strX, strY);
   let url = Z.toString(10).toUpperCase();
   for (let i = 0; i < pathDepth; i += 1) {
@@ -15,6 +17,7 @@ function getPath(X, Y, Z, pathDepth) {
   return {
     dirPath: url,
     filename: strX[pathDepth] + strY[pathDepth],
+    numTile: (Y % slabSize.height) * slabSize.width + (X % slabSize.width),
   };
 }
 
