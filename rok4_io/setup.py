84c1dc578864753aee10387741b1b07e8ccb2b94
Ana-Maria Rosu <ana-maria.rosu@ign.fr>
2021-05-28 21:07:52 +0200

    fix: lint py setup

--- rok4_io/setup.py
+++ rok4_io/setup.py
@@ -2,16 +2,16 @@
 import distutils
 from distutils.core import setup, Extension
 
-setup(name = "ROK4_io",
-    version = "0.1",
-    url = 'https://github.com/ign-packo/PackO',
-    author = "IGN",
-    ext_modules = [
+setup(name="ROK4_io",
+      version="0.1",
+      url='https://github.com/ign-packo/PackO',
+      author="IGN",
+      ext_modules=[
         Extension(
-            "_rok4_io", sources = ["rok4_io.i", "ImageIO.cpp"],
-            swig_opts=['-Wall','-c++','-py3'],
-            libraries = ['jpeg', 'png']
+            "_rok4_io",
+            sources=["rok4_io.i", "ImageIO.cpp"],
+            swig_opts=['-Wall', '-c++', '-py3'],
+            libraries=['jpeg', 'png']
         )
-    ],
-    py_modules = ['rok4_io']
-)
+      ],
+      py_modules=['rok4_io'])
