b91316b7bd127de80eec92874872953e45863073
François Toromanoff <francois.toromanoff@ign.fr>
2021-06-28 17:20:15 +0200

    refactor(monitor): use of asynch/await for Fetcher() -add tab-

--- itowns/index.js
+++ itowns/index.js
@@ -55,239 +55,239 @@ async function main() {
   try {
     const overviews = await itowns.Fetcher.json(`${apiUrl}/json/overviews`);
 
-  // Define projection that we will use (taken from https://epsg.io/3946, Proj4js section)
-  const crs = `${overviews.crs.type}:${overviews.crs.code}`;
-  if (crs !== 'EPSG:4326' && overviews.crs.proj4Definition) {
-    itowns.CRS.defs(crs, overviews.crs.proj4Definition);
-  } else {
-    throw new Error('EPSG proj4.defs not defined in overviews.json');
-  }
-
-  // limite du crs
-  const xOrigin = overviews.crs.boundingBox.xmin;
-  const yOrigin = overviews.crs.boundingBox.ymax;
-
-  // limite du dataSet
-  const xmin = overviews.dataSet.boundingBox.LowerCorner[0];
-  const xmax = overviews.dataSet.boundingBox.UpperCorner[0];
-  const ymin = overviews.dataSet.boundingBox.LowerCorner[1];
-  const ymax = overviews.dataSet.boundingBox.UpperCorner[1];
-
-  // Define geographic extent of level 0 : CRS, min/max X, min/max Y
-  const { resolution } = overviews;
-  const resolutionLv0 = resolution * 2 ** overviews.level.max;
-  const extent = new itowns.Extent(
-    crs,
-    xOrigin, xOrigin + (overviews.tileSize.width * resolutionLv0),
-    yOrigin - (overviews.tileSize.height * resolutionLv0), yOrigin,
-  );
-
-  const dezoomInitial = 4;// to define
-  const resolInit = resolution * 2 ** dezoomInitial;
-  const xcenter = (xmin + xmax) * 0.5;
-  const ycenter = (ymin + ymax) * 0.5;
-
-  // `viewerDiv` will contain iTowns' rendering area (`<canvas>`)
-  const viewerDiv = document.getElementById('viewerDiv');
-
-  const placement = new itowns.Extent(
-    crs,
-    xcenter - viewerDiv.clientWidth * resolInit * 0.5,
-    xcenter + viewerDiv.clientWidth * resolInit * 0.5,
-    ycenter - viewerDiv.clientHeight * resolInit * 0.5,
-    ycenter + viewerDiv.clientHeight * resolInit * 0.5,
-  );
-
-  const resolLvMax = resolution * 2 ** (overviews.level.max - overviews.dataSet.level.max);
-  const resolLvMin = resolution * 2 ** (overviews.level.max - overviews.dataSet.level.min);
-
-  // Instanciate PlanarView*
-  const zoomFactor = 2;// customizable
-  const view = new itowns.PlanarView(viewerDiv, extent, {
-    camera: {
-      type: itowns.CAMERA_TYPE.ORTHOGRAPHIC,
-    },
-    placement,
-    maxSubdivisionLevel: 30,
-    disableSkirt: true,
-    controls: {
-      enableSmartTravel: false,
-      zoomFactor,
-      maxResolution: resolLvMax * zoomFactor ** -1,
-      minResolution: resolLvMin * zoomFactor,
-    },
-  });
-
-  setupLoadingScreen(viewerDiv, view);
-
-  view.isDebugMode = true;
-  const menuGlobe = new GuiTools('menuDiv', view);
-  menuGlobe.gui.width = 300;
-
-  const opacity = {
-    ortho: 1,
-    graph: 0.2,
-    opi: 0.5,
-  };
-
-  const layer = {};
-  ['ortho', 'graph', 'opi'].forEach((id) => {
-    layer[id] = {
-      name: id.charAt(0).toUpperCase() + id.slice(1),
-      config: {
-        source: {
-          url: `${apiUrl}/wmts`,
-          crs,
-          format: 'image/png',
-          name: id,
-          tileMatrixSet: overviews.identifier,
-          tileMatrixSetLimits: overviews.dataSet.limits,
-        },
-        opacity: opacity[id],
-      },
-    };
-    layer[id].config.source = new itowns.WMTSSource(layer[id].config.source);
-    layer[id].config.source.extentInsideLimit = function extentInsideLimit() {
-      return true;
-    };
-
-    layer[id].colorLayer = new itowns.ColorLayer(layer[id].name, layer[id].config);
-    if (id === 'opi') layer[id].colorLayer.visible = false;
-    view.addLayer(layer[id].colorLayer);// .then(menuGlobe.addLayerGUI.bind(menuGlobe));
-  });
-  itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Ortho', 0);
-  itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Opi', 1);
-  itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Graph', 2);
-  // Et ouvrir l'onglet "Color Layers" par defaut ?
-
-  // Request redraw
-  view.notifyChange();
-
-  const saisie = new Saisie(view, layer, apiUrl);
-  saisie.cliche = 'unknown';
-  saisie.message = '';
-  saisie.coord = `${xcenter.toFixed(2)},${ycenter.toFixed(2)}`;
-  saisie.color = [0, 0, 0];
-  saisie.controllers = {};
-  saisie.controllers.select = menuGlobe.gui.add(saisie, 'select');
-  saisie.controllers.cliche = menuGlobe.gui.add(saisie, 'cliche');
-  saisie.controllers.cliche.listen().domElement.parentElement.style.pointerEvents = 'none';
-  saisie.controllers.coord = menuGlobe.gui.add(saisie, 'coord');
-  saisie.controllers.coord.listen();// .domElement.parentElement.style.pointerEvents = 'none';
-  saisie.controllers.polygon = menuGlobe.gui.add(saisie, 'polygon');
-  saisie.controllers.undo = menuGlobe.gui.add(saisie, 'undo');
-  saisie.controllers.redo = menuGlobe.gui.add(saisie, 'redo');
-  if (process.env.NODE_ENV === 'development') saisie.controllers.clear = menuGlobe.gui.add(saisie, 'clear');
-  saisie.controllers.message = menuGlobe.gui.add(saisie, 'message');
-  saisie.controllers.message.listen().domElement.parentElement.style.pointerEvents = 'none';
-
-  viewerDiv.focus();
-
-  view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, () => {
-    // eslint-disable-next-line no-console
-    console.info('View initialized');
-    updateScaleWidget(view, resolution);
-  });
-  view.addEventListener(itowns.PLANAR_CONTROL_EVENT.MOVED, () => {
-    // eslint-disable-next-line no-console
-    console.info('View moved');
-    if (view.controls.state === -1) {
-      updateScaleWidget(view, resolution);
-    }
-  });
-
-  viewerDiv.addEventListener('mousemove', (ev) => {
-    ev.preventDefault();
-    saisie.mousemove(ev);
-    return false;
-  }, false);
-  viewerDiv.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    saisie.click(ev);
-    return false;
-  }, false);
-  viewerDiv.addEventListener('mousedown', (ev) => {
-    if (ev.button === 1) {
-      console.log('middle button clicked');
-      view.controls.initiateDrag();
-      view.controls.updateMouseCursorType();
-    }
-  });
-
-  saisie.controllers.coord.onChange(() => {
-    if (!checkCoordString(saisie.coord)) {
-      saisie.message = 'Coordonnees non valides';
+    // Define projection that we will use (taken from https://epsg.io/3946, Proj4js section)
+    const crs = `${overviews.crs.type}:${overviews.crs.code}`;
+    if (crs !== 'EPSG:4326' && overviews.crs.proj4Definition) {
+      itowns.CRS.defs(crs, overviews.crs.proj4Definition);
     } else {
-      saisie.message = '';
+      throw new Error('EPSG proj4.defs not defined in overviews.json');
     }
-    return false;
-  });
 
-  saisie.controllers.coord.onFinishChange(() => {
-    const coords = checkCoordString(saisie.coord);
-    if (coords) {
+    // limite du crs
+    const xOrigin = overviews.crs.boundingBox.xmin;
+    const yOrigin = overviews.crs.boundingBox.ymax;
+
+    // limite du dataSet
+    const xmin = overviews.dataSet.boundingBox.LowerCorner[0];
+    const xmax = overviews.dataSet.boundingBox.UpperCorner[0];
+    const ymin = overviews.dataSet.boundingBox.LowerCorner[1];
+    const ymax = overviews.dataSet.boundingBox.UpperCorner[1];
+
+    // Define geographic extent of level 0 : CRS, min/max X, min/max Y
+    const { resolution } = overviews;
+    const resolutionLv0 = resolution * 2 ** overviews.level.max;
+    const extent = new itowns.Extent(
+      crs,
+      xOrigin, xOrigin + (overviews.tileSize.width * resolutionLv0),
+      yOrigin - (overviews.tileSize.height * resolutionLv0), yOrigin,
+    );
+
+    const dezoomInitial = 4;// to define
+    const resolInit = resolution * 2 ** dezoomInitial;
+    const xcenter = (xmin + xmax) * 0.5;
+    const ycenter = (ymin + ymax) * 0.5;
+
+    // `viewerDiv` will contain iTowns' rendering area (`<canvas>`)
+    const viewerDiv = document.getElementById('viewerDiv');
+
+    const placement = new itowns.Extent(
+      crs,
+      xcenter - viewerDiv.clientWidth * resolInit * 0.5,
+      xcenter + viewerDiv.clientWidth * resolInit * 0.5,
+      ycenter - viewerDiv.clientHeight * resolInit * 0.5,
+      ycenter + viewerDiv.clientHeight * resolInit * 0.5,
+    );
+
+    const resolLvMax = resolution * 2 ** (overviews.level.max - overviews.dataSet.level.max);
+    const resolLvMin = resolution * 2 ** (overviews.level.max - overviews.dataSet.level.min);
+
+    // Instanciate PlanarView*
+    const zoomFactor = 2;// customizable
+    const view = new itowns.PlanarView(viewerDiv, extent, {
+      camera: {
+        type: itowns.CAMERA_TYPE.ORTHOGRAPHIC,
+      },
+      placement,
+      maxSubdivisionLevel: 30,
+      disableSkirt: true,
+      controls: {
+        enableSmartTravel: false,
+        zoomFactor,
+        maxResolution: resolLvMax * zoomFactor ** -1,
+        minResolution: resolLvMin * zoomFactor,
+      },
+    });
+
+    setupLoadingScreen(viewerDiv, view);
+
+    view.isDebugMode = true;
+    const menuGlobe = new GuiTools('menuDiv', view);
+    menuGlobe.gui.width = 300;
+
+    const opacity = {
+      ortho: 1,
+      graph: 0.2,
+      opi: 0.5,
+    };
+
+    const layer = {};
+    ['ortho', 'graph', 'opi'].forEach((id) => {
+      layer[id] = {
+        name: id.charAt(0).toUpperCase() + id.slice(1),
+        config: {
+          source: {
+            url: `${apiUrl}/wmts`,
+            crs,
+            format: 'image/png',
+            name: id,
+            tileMatrixSet: overviews.identifier,
+            tileMatrixSetLimits: overviews.dataSet.limits,
+          },
+          opacity: opacity[id],
+        },
+      };
+      layer[id].config.source = new itowns.WMTSSource(layer[id].config.source);
+      layer[id].config.source.extentInsideLimit = function extentInsideLimit() {
+        return true;
+      };
+
+      layer[id].colorLayer = new itowns.ColorLayer(layer[id].name, layer[id].config);
+      if (id === 'opi') layer[id].colorLayer.visible = false;
+      view.addLayer(layer[id].colorLayer);// .then(menuGlobe.addLayerGUI.bind(menuGlobe));
+    });
+    itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Ortho', 0);
+    itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Opi', 1);
+    itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Graph', 2);
+    // Et ouvrir l'onglet "Color Layers" par defaut ?
+
+    // Request redraw
+    view.notifyChange();
+
+    const saisie = new Saisie(view, layer, apiUrl);
+    saisie.cliche = 'unknown';
+    saisie.message = '';
+    saisie.coord = `${xcenter.toFixed(2)},${ycenter.toFixed(2)}`;
+    saisie.color = [0, 0, 0];
+    saisie.controllers = {};
+    saisie.controllers.select = menuGlobe.gui.add(saisie, 'select');
+    saisie.controllers.cliche = menuGlobe.gui.add(saisie, 'cliche');
+    saisie.controllers.cliche.listen().domElement.parentElement.style.pointerEvents = 'none';
+    saisie.controllers.coord = menuGlobe.gui.add(saisie, 'coord');
+    saisie.controllers.coord.listen();// .domElement.parentElement.style.pointerEvents = 'none';
+    saisie.controllers.polygon = menuGlobe.gui.add(saisie, 'polygon');
+    saisie.controllers.undo = menuGlobe.gui.add(saisie, 'undo');
+    saisie.controllers.redo = menuGlobe.gui.add(saisie, 'redo');
+    if (process.env.NODE_ENV === 'development') saisie.controllers.clear = menuGlobe.gui.add(saisie, 'clear');
+    saisie.controllers.message = menuGlobe.gui.add(saisie, 'message');
+    saisie.controllers.message.listen().domElement.parentElement.style.pointerEvents = 'none';
+
+    viewerDiv.focus();
+
+    view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, () => {
+    // eslint-disable-next-line no-console
+      console.info('View initialized');
+      updateScaleWidget(view, resolution);
+    });
+    view.addEventListener(itowns.PLANAR_CONTROL_EVENT.MOVED, () => {
+    // eslint-disable-next-line no-console
+      console.info('View moved');
+      if (view.controls.state === -1) {
+        updateScaleWidget(view, resolution);
+      }
+    });
+
+    viewerDiv.addEventListener('mousemove', (ev) => {
+      ev.preventDefault();
+      saisie.mousemove(ev);
+      return false;
+    }, false);
+    viewerDiv.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      saisie.click(ev);
+      return false;
+    }, false);
+    viewerDiv.addEventListener('mousedown', (ev) => {
+      if (ev.button === 1) {
+        console.log('middle button clicked');
+        view.controls.initiateDrag();
+        view.controls.updateMouseCursorType();
+      }
+    });
+
+    saisie.controllers.coord.onChange(() => {
+      if (!checkCoordString(saisie.coord)) {
+        saisie.message = 'Coordonnees non valides';
+      } else {
+        saisie.message = '';
+      }
+      return false;
+    });
+
+    saisie.controllers.coord.onFinishChange(() => {
+      const coords = checkCoordString(saisie.coord);
+      if (coords) {
+        itowns.CameraUtils.transformCameraToLookAtTarget(
+          view,
+          view.camera.camera3D,
+          {
+            coord: new itowns.Coordinates(crs, coords[0], coords[1]),
+            heading: 0,
+          },
+        );
+      }
+      saisie.message = '';
+      return false;
+    });
+
+    window.addEventListener('keydown', (ev) => {
+      saisie.keydown(ev);
+      return false;
+    });
+    window.addEventListener('keyup', (ev) => {
+      saisie.keyup(ev);
+      return false;
+    });
+
+    document.getElementById('recenterBtn').addEventListener('click', () => {
+    // bug itowns...
+    // itowns.CameraUtils.animateCameraToLookAtTarget( ... )
       itowns.CameraUtils.transformCameraToLookAtTarget(
         view,
         view.camera.camera3D,
         {
-          coord: new itowns.Coordinates(crs, coords[0], coords[1]),
+          coord: new itowns.Coordinates(crs, xcenter, ycenter),
           heading: 0,
         },
       );
-    }
-    saisie.message = '';
-    return false;
-  });
+      return false;
+    }, false);
+    document.getElementById('zoomInBtn').addEventListener('click', () => {
+      console.log('Zoom-In');
+      if (view.getPixelsToMeters() > resolLvMax) {
+        view.camera.camera3D.zoom *= 2;
+        view.camera.camera3D.updateProjectionMatrix();
+        view.notifyChange(view.camera.camera3D);
+        updateScaleWidget(view, resolution);
+      }
+      return false;
+    });
+    document.getElementById('zoomOutBtn').addEventListener('click', () => {
+      console.log('Zoom-Out');
+      if (view.getPixelsToMeters() < resolLvMin) {
+        view.camera.camera3D.zoom *= 0.5;
+        view.camera.camera3D.updateProjectionMatrix();
+        view.notifyChange(view.camera.camera3D);
+        updateScaleWidget(view, resolution);
+      }
+      return false;
+    });
 
-  window.addEventListener('keydown', (ev) => {
-    saisie.keydown(ev);
-    return false;
-  });
-  window.addEventListener('keyup', (ev) => {
-    saisie.keyup(ev);
-    return false;
-  });
-
-  document.getElementById('recenterBtn').addEventListener('click', () => {
-    // bug itowns...
-    // itowns.CameraUtils.animateCameraToLookAtTarget( ... )
-    itowns.CameraUtils.transformCameraToLookAtTarget(
-      view,
-      view.camera.camera3D,
-      {
-        coord: new itowns.Coordinates(crs, xcenter, ycenter),
-        heading: 0,
-      },
-    );
-    return false;
-  }, false);
-  document.getElementById('zoomInBtn').addEventListener('click', () => {
-    console.log('Zoom-In');
-    if (view.getPixelsToMeters() > resolLvMax) {
-      view.camera.camera3D.zoom *= 2;
-      view.camera.camera3D.updateProjectionMatrix();
-      view.notifyChange(view.camera.camera3D);
-      updateScaleWidget(view, resolution);
-    }
-    return false;
-  });
-  document.getElementById('zoomOutBtn').addEventListener('click', () => {
-    console.log('Zoom-Out');
-    if (view.getPixelsToMeters() < resolLvMin) {
-      view.camera.camera3D.zoom *= 0.5;
-      view.camera.camera3D.updateProjectionMatrix();
-      view.notifyChange(view.camera.camera3D);
-      updateScaleWidget(view, resolution);
-    }
-    return false;
-  });
-
-  const helpContent = document.getElementById('help-content');
-  helpContent.style.visibility = 'hidden';
-  document.getElementById('help').addEventListener('click', () => {
-    helpContent.style.visibility = (helpContent.style.visibility === 'hidden') ? 'visible' : 'hidden';
-  });
-} catch (err) {
+    const helpContent = document.getElementById('help-content');
+    helpContent.style.visibility = 'hidden';
+    document.getElementById('help').addEventListener('click', () => {
+      helpContent.style.visibility = (helpContent.style.visibility === 'hidden') ? 'visible' : 'hidden';
+    });
+  } catch (err) {
     console.log(`${err.name}: ${err.message}`);
     if (`${err.name}: ${err.message}` === 'TypeError: Failed to fetch') {
       const newApiUrl = window.prompt(`API non accessible à l'adresse renseignée (${apiUrl}). Veuillez entrer une adresse valide :`, apiUrl);
