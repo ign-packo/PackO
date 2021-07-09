392cd36834287db457fc58146881f7e6cb01c208
Nathan Lenglet <nathan.lenglet@ign.fr>
2021-07-09 15:16:07 +0200

    add buildvrt & polygonize steps to script

--- scripts/vectorise_graph.py
+++ scripts/vectorise_graph.py
@@ -4,7 +4,6 @@ Script de vectorisation du graphe à partir d'un cache d'images COG
 """
 import os
 import argparse
-from osgeo import gdal
 
 
 def read_args():
@@ -12,9 +11,13 @@ def read_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", required=True, help="input cache folder")
-    parser.add_argument("-o", "--output", help="output file (default : .\list_files.txt)", default="list_files")
+    parser.add_argument(
+        "-o", "--output", help="output folder (default : .)", default="."
+    )
 
-    parser.add_argument("-v", "--verbose", help="verbose (default: 0)", type=int, default=0)
+    parser.add_argument(
+        "-v", "--verbose", help="verbose (default: 0)", type=int, default=0
+    )
     args = parser.parse_args()
 
     if args.verbose >= 1:
@@ -25,13 +28,40 @@ def read_args():
 
 args = read_args()
 
-graph_dir = args.input+"/graph"
-fOut = open(args.output+"_"+os.path.basename(args.input).split('.')[0]+".txt", "w")
+pathOut = os.path.join(args.output, os.path.basename(args.input).split(".")[0] + ".txt")
 
+graph_dir = args.input + "/graph"
+fOut = open(pathOut, "w")
+
+# on parcourt le repertoire graph du cache pour recuperer l'ensemble des images de graphe
 for (root, dirs, files) in os.walk(graph_dir):
     for file in files:
         file = os.path.join(root, file)
         fOut.write(file + "\n")
-        print(file)
 
 fOut.close()
+
+# on construit un vrt a partir de la liste des images recuperee precedemment
+GDAL_BIN = "C:\\Users\\nlenglet\\.conda\\envs\\packo\\Library\\bin"
+buildvrt_exe = os.path.join(GDAL_BIN, "gdalbuildvrt")
+
+cmd_buildvrt = (
+    buildvrt_exe + " -input_file_list " + pathOut + " " + pathOut.split(".")[0] + ".vrt"
+)
+
+os.system(cmd_buildvrt)
+
+# on vectorise le graphe à partir du vrt
+GDAL_SCRIPT_ROOT = "C:\\Users\\nlenglet\\.conda\\envs\\packo\\Scripts"
+polygonize_exe = os.path.join(GDAL_SCRIPT_ROOT, "gdal_polygonize.py")
+
+cmd_polygonize = (
+    "python "
+    + polygonize_exe
+    + " "
+    + pathOut.split(".")[0]
+    + ".vrt "
+    + pathOut.split(".")[0]
+    + '.geojson -f "Geojson"'
+)
+os.system(cmd_polygonize)
