f0fd76bb3726df98dadef7b60cdea25c73151481
greg maillet <gregoire.maillet@ign.fr>
2021-07-16 19:03:16 +0200

    feat(API): add delete and rebase for branches

--- middlewares/branch.js
+++ middlewares/branch.js
@@ -1,4 +1,9 @@
+const debug = require('debug')('branch');
 const { matchedData } = require('express-validator');
+const path = require('path');
+const fs = require('fs');
+const cog = require('../cog_path.js');
+const pacthMiddlewares = require('./patch');
 
 function validBranch(req, res, next) {
   const params = matchedData(req);
@@ -13,6 +18,87 @@ function validBranch(req, res, next) {
   return next();
 }
 
+function createCopy(newBranch, selectedBranch, overviews) {
+  debug('~~~createCopy~~~');
+  const { id } = selectedBranch;
+
+  // on cherche les tuiles de façon unique (pour ne passer qu'une fois dans chaque tuile)
+  let selectedSlabs = new Set();
+  selectedBranch.activePatches.features.forEach((feature) => {
+    feature.properties.slabs.forEach((slab) => {
+      selectedSlabs.add(JSON.stringify(slab));
+    });
+  });
+  selectedSlabs = Array.from(selectedSlabs).map((slab) => JSON.parse(slab));
+
+  debug('tuiles a copier : ', selectedSlabs);
+  selectedSlabs.forEach((slab) => {
+    const cogPath = cog.getSlabPath(
+      slab.x,
+      slab.y,
+      slab.z,
+      overviews,
+    );
+    const graphDir = path.join(global.dir_cache, 'graph', cogPath.dirPath);
+    const orthoDir = path.join(global.dir_cache, 'ortho', cogPath.dirPath);
+    const opiDir = path.join(global.dir_cache, 'opi', cogPath.dirPath);
+    debug(orthoDir);
+    const arrayLinkOrtho = fs.readdirSync(orthoDir).filter(
+      (filename) => (filename.startsWith(`${id}_${cogPath.filename}`)),
+    );
+    const regex = new RegExp(`^${id}_`);
+    debug(regex);
+    arrayLinkOrtho.forEach((file) => {
+      const newName = file.replace(regex, `${newBranch.id}_`);
+      debug('copy ', file, newName);
+      fs.copyFileSync(path.join(orthoDir, file), path.join(orthoDir, newName));
+    });
+    debug(graphDir);
+    const arrayLinkGraph = fs.readdirSync(graphDir).filter(
+      (filename) => (filename.startsWith(`${id}_${cogPath.filename}`)),
+    );
+    arrayLinkGraph.forEach((file) => {
+      const newName = file.replace(regex, `${newBranch.id}_`);
+      debug('copy ', file, newName);
+      fs.copyFileSync(path.join(graphDir, file), path.join(graphDir, newName));
+    });
+    debug(opiDir);
+    const arrayLinkOpi = fs.readdirSync(opiDir).filter(
+      (filename) => (filename.startsWith(`${id}_${cogPath.filename}`)),
+    );
+    arrayLinkOpi.forEach((file) => {
+      const newName = file.replace(regex, `${newBranch.id}_`);
+      debug('copy ', file, newName);
+      fs.copyFileSync(path.join(opiDir, file), path.join(opiDir, newName));
+    });
+  });
+  // on transfert les patchs
+  /* eslint-disable no-param-reassign */
+  newBranch.activePatches.features = selectedBranch.activePatches.features;
+  /* eslint-enable no-param-reassign */
+  debug('features in activePatches:', newBranch.activePatches.features.length);
+  debug('~~~fin createCopy~~~');
+}
+
+function applyAllPatches(newBranch, features, overviews) {
+  return features.reduce((previousPromise, nextFeature) => previousPromise.then(() => {
+    /* eslint-disable no-param-reassign */
+    nextFeature.properties.patchId = newBranch.activePatches.features.length + 1;
+    /* eslint-enable no-param-reassign */
+    debug('application de ', nextFeature);
+    return pacthMiddlewares.applyPatch(
+      [nextFeature],
+      overviews,
+      nextFeature.properties.slabs,
+      newBranch,
+      nextFeature.properties.patchId,
+    );
+  }),
+  Promise.resolve());
+}
+
 module.exports = {
   validBranch,
+  createCopy,
+  applyAllPatches,
 };
--- routes/branch.js
+++ routes/branch.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const debug = require('debug')('branch');
 const router = require('express').Router();
-const { matchedData, query } = require('express-validator');
-
+const turf = require('@turf/turf');
+const { matchedData, query, param } = require('express-validator');
 const validateParams = require('../paramValidation/validateParams');
 const createErrMsg = require('../paramValidation/createErrMsg');
+const branchMiddelwares = require('../middlewares/branch');
+const cog = require('../cog_path.js');
 
 router.get('/branches', (req, res) => {
   debug('~~~get branches~~~');
@@ -66,6 +68,181 @@ router.post('/branch', [
   fs.writeFileSync(path.join(global.dir_cache, 'branches.json'), JSON.stringify(req.app.branches, null, 4));
 
   res.status(200).send(JSON.stringify({ name, id: largestId + 1 }));
+});
+
+router.delete('/branch/:idBranch', [
+  param('idBranch')
+    .exists().withMessage(createErrMsg.missingParameter('id'))
+    .isInt({ min: 0 })
+    .withMessage(createErrMsg.invalidParameter('id')),
+],
+validateParams,
+branchMiddelwares.validBranch,
+(req, res) => {
+  const { overviews } = req.app;
+  const params = matchedData(req);
+  const id = Number(params.idBranch);
+  debug('~~~delete branch~~~');
+  if (req.app.branches.length <= 1) {
+    res.status(406).send('it is not possible to delete the last branch');
+    return;
+  }
+  debug('Id de la branche a supprimer : ', id);
+
+  // on purge le cache (actif et inactif)
+  const { selectedBranch } = req;
+  // on cherche les tuiles de façon unique (pour ne passer qu'une fois dans chaque tuile)
+  let selectedSlabs = new Set();
+  selectedBranch.activePatches.features.forEach((feature) => {
+    debug('On purge : ', feature);
+    feature.properties.slabs.forEach((slab) => {
+      selectedSlabs.add(JSON.stringify(slab));
+    });
+  });
+  selectedBranch.unactivePatches.features.forEach((feature) => {
+    debug('On purge : ', feature);
+    feature.properties.slabs.forEach((slab) => {
+      selectedSlabs.add(JSON.stringify(slab));
+    });
+  });
+  selectedSlabs = Array.from(selectedSlabs).map((slab) => JSON.parse(slab));
+  debug('tuiles a pruger : ', selectedSlabs);
+  selectedSlabs.forEach((slab) => {
+    const cogPath = cog.getSlabPath(slab.x, slab.y, slab.z, overviews.pathDepth);
+    const graphDir = path.join(global.dir_cache, 'graph', cogPath.dirPath);
+    const orthoDir = path.join(global.dir_cache, 'ortho', cogPath.dirPath);
+    const opiDir = path.join(global.dir_cache, 'opi', cogPath.dirPath);
+    debug(orthoDir);
+    const arrayLinkOrtho = fs.readdirSync(orthoDir).filter(
+      (filename) => (filename.startsWith(`${id}_${cogPath.filename}`)),
+    );
+    arrayLinkOrtho.forEach((file) => fs.unlinkSync(
+      path.join(orthoDir, file),
+    ));
+    debug(graphDir);
+    const arrayLinkGraph = fs.readdirSync(graphDir).filter(
+      (filename) => (filename.startsWith(`${id}_${cogPath.filename}`)),
+    );
+    arrayLinkGraph.forEach((file) => fs.unlinkSync(
+      path.join(graphDir, file),
+    ));
+    debug(opiDir);
+    const arrayLinkOpi = fs.readdirSync(opiDir).filter(
+      (filename) => (filename.startsWith(`${id}_${cogPath.filename}`)),
+    );
+    arrayLinkOpi.forEach((file) => fs.unlinkSync(
+      path.join(opiDir, file),
+    ));
+  });
+  // on fait une copie de sauvegarde de la branche
+  const timestamp = new Date().getTime();
+  fs.writeFileSync(path.join(global.dir_cache, `deleted_branche_${timestamp}.json`), JSON.stringify(selectedBranch, null, 4));
+  // on supprime la branche
+  req.app.branches = req.app.branches.filter((item) => item.id !== id);
+  fs.writeFileSync(path.join(global.dir_cache, 'branches.json'), JSON.stringify(req.app.branches, null, 4));
+  res.status(200).send(`branch ${id} deleted`);
+});
+
+router.post('/rebase', [
+  query('name')
+    .exists().withMessage(createErrMsg.missingParameter('name')),
+  query('firstId')
+    .exists().withMessage(createErrMsg.missingParameter('firstId'))
+    .isInt({ min: 0 })
+    .withMessage(createErrMsg.invalidParameter('firstId')),
+  query('secondId')
+    .exists().withMessage(createErrMsg.missingParameter('secondId'))
+    .isInt({ min: 0 })
+    .withMessage(createErrMsg.invalidParameter('secondId')),
+],
+validateParams,
+(req, res) => {
+  const { overviews } = req.app;
+  const params = matchedData(req);
+  const { name } = params;
+  const firstId = Number(params.firstId);
+  const secondId = Number(params.secondId);
+  debug('~~~post rebase~~~');
+  debug(firstId, ' + ', secondId, ' -> ', name);
+  // on verifie que les deux branches sont valides
+  const firstBranches = req.app.branches.filter((item) => item.id === Number(firstId));
+  const secondBranches = req.app.branches.filter((item) => item.id === Number(secondId));
+  if (firstBranches.length === 0) {
+    res.status(400).json({
+      errors: 'branch does not exist',
+    });
+    return;
+  }
+  const [firstBranch] = firstBranches;
+  if (secondBranches.length === 0) {
+    res.status(400).json({
+      errors: 'branch does not exist',
+    });
+    return;
+  }
+  const [secondBranch] = secondBranches;
+  // on vérifie si le nom est deja pris
+  let largestId = 0;
+  let ok = true;
+  req.app.branches.forEach((branch) => {
+    largestId = Math.max(largestId, branch.id);
+    if (branch.name === name) {
+      ok = false;
+    }
+  });
+  if (!ok) {
+    res.status(406).send('A branch with this name already exist');
+    return;
+  }
+  // on crée la nouvelle branche
+  const newBranch = {
+    id: largestId + 1,
+    name,
+    activePatches: {
+      type: 'FeatureCollection',
+      name: 'annotation',
+      crs: {
+        type: 'name',
+        properties: {
+          name: 'urn:ogc:def:crs:EPSG::2154',
+        },
+      },
+      features: [],
+    },
+    unactivePatches: {
+      type: 'FeatureCollection',
+      name: 'annotation',
+      crs: {
+        type: 'name',
+        properties: {
+          name: 'urn:ogc:def:crs:EPSG::2154',
+        },
+      },
+      features: [],
+    },
+  };
+  // on recupere l'historique de la premiere branche
+  branchMiddelwares.createCopy(newBranch, firstBranch, overviews);
+
+  const features = [...secondBranch.activePatches.features];
+
+  // on detecte les conflits entre les deux branches
+  newBranch.conflicts = [];
+  secondBranch.activePatches.features.forEach((feature) => {
+    const fc = turf.featureCollection([feature]);
+    if (turf.booleanIntersects(fc, firstBranch.activePatches)) {
+      newBranch.conflicts.push(feature);
+    }
+  });
+
+  // on applique l'historique de la deuxième branche
+  branchMiddelwares.applyAllPatches(newBranch, features, overviews).then(() => {
+    debug('finalisation');
+    // on insere la nouvelle branche
+    req.app.branches.push(newBranch);
+    fs.writeFileSync(path.join(global.dir_cache, 'branches.json'), JSON.stringify(req.app.branches, null, 4));
+    res.status(200).send(JSON.stringify({ name, id: largestId + 1 }));
+  });
 });
 
 module.exports = router;
