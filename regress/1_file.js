43803d422a1eebd558646b5af57813c9ed853e93
François Toromanoff <francois.toromanoff@ign.fr>
2021-09-23 14:02:43 +0200

    feat(API): road File -> connection to DB

--- regress/1_file.js
+++ regress/1_file.js
@@ -5,6 +5,8 @@ chai.use(require('chai-http'));
 const should = chai.should();
 const app = require('..');
 
+const cachePath = 'cache_test';
+
 describe('Files', () => {
   after((done) => {
     app.server.close();
@@ -16,6 +18,7 @@ describe('Files', () => {
       it('should return a json file', (done) => {
         chai.request(app)
           .get('/json/overviews')
+          .query({ cachePath })
           .end((err, res) => {
             should.not.exist(err);
             res.should.be.a('object');
@@ -28,16 +31,33 @@ describe('Files', () => {
           });
       });
     });
-    describe('filetype = test', () => {
+    describe('filetype = test (test.json is not a file)', () => {
       it('should return an error', (done) => {
         chai.request(app)
           .get('/json/test')
+          .query({ cachePath })
           .end((err, res) => {
             should.not.exist(err);
             res.should.be.a('object');
             res.should.have.status(404);
             res.body.should.be.a('object');
             res.body.should.have.property('status').equal("Le fichier demandé (test.json) n'existe pas");
+
+            done();
+          });
+      });
+    });
+    describe('filetype = overviews and cachePath = testPath (non valide)', () => {
+      it('should return an error', (done) => {
+        chai.request(app)
+          .get('/json/overviews')
+          .query({ cachePath: 'testPath' })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.be.a('object');
+            res.should.have.status(404);
+            res.body.should.be.a('object');
+            res.body.should.have.property('status').equal("Le dossier demandé (testPath) n'existe pas");
 
             done();
           });
