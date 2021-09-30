7b85e51e4b270b57e48343120a50c7c911b672e7
François Toromanoff <francois.toromanoff@ign.fr>
2021-09-30 16:27:53 +0200

    fix(API): deprecated body-parser

--- serveur.js
+++ serveur.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const os = require('os');
 
 const swaggerUi = require('swagger-ui-express');
@@ -40,6 +40,7 @@ const file = require('./routes/file');
 const patch = require('./routes/patch');
 const { misc, gitVersion } = require('./routes/misc');
 const branch = require('./routes/branch');
+const vector = require('./routes/vector');
 
 try {
   // desactive la mise en cache des images par le navigateur - OK Chrome/Chromium et Firefox
@@ -90,7 +91,7 @@ try {
   }
 
   app.use(cors());
-  app.use(bodyParser.json());
+  app.use(express.json({ limit: '50mb' }));
 
   app.use((req, res, next) => {
     debugServer(req.method, ' ', req.path, ' ', req.query.REQUEST || '');
@@ -114,6 +115,7 @@ try {
   app.use('/', patch);
   app.use('/', misc);
   app.use('/', branch);
+  app.use('/', vector);
 
   app.use('/itowns', express.static('itowns'));
 
