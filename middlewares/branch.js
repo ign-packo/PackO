093ce00481ff2e519caeedc18a20830697796aff
greg maillet <gregoire.maillet@ign.fr>
2021-10-14 12:11:43 +0200

    fix branch delete error 406

--- middlewares/branch.js
+++ middlewares/branch.js
@@ -113,7 +113,7 @@ async function deleteBranch(req, _res, next) {
     debug(error);
     req.error = {
       msg: `Branch '${idBranch}' can't be deleted`,
-      code: 400,
+      code: 406,
       function: 'deleteBranch',
     };
   }
