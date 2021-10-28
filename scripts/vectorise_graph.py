1ecd6e064b02270f8c32ece058f0b368e883fda1
Nathan Lenglet <nathan.lenglet@ign.fr>
2021-10-28 16:13:02 +0200

    refactor: cleaning code
    refactor: clean code

--- scripts/vectorise_graph.py
+++ scripts/vectorise_graph.py
@@ -5,9 +5,9 @@ Script de vectorisation du graphe à partir d'un cache d'images COG
 import os
 import argparse
 import json
-from cache_def import get_slab_path
 import platform
 import time
+from cache_def import get_slab_path
 
 def read_args():
     """Gestion des arguments"""
@@ -58,28 +58,28 @@ print("Updated input path relative to working dir: '" + args.input + "'")
 if not os.path.exists(args.input):
     raise SystemExit("Directory " + args.input + " does not exist.")
 
-tStart = time.perf_counter()
+t_start = time.perf_counter()
 
-tStartPrep = time.perf_counter()
+t_start_prep = time.perf_counter()
 
 # lecture du fichier overviews pour recuperer les infos du cache
 fileOverviews = open(os.path.join(args.input, "overviews.json"))
 overviews = json.load(fileOverviews)
 fileOverviews.close()
 
-pathDepth = overviews['pathDepth']
+path_depth = overviews['pathDepth']
 level = overviews['level']['max']
 
 # valeur par defaut pour verifier l'existance de la branche desiree apres recherche
 id_branch = args.branch
 
 # on recupere les infos concernant les patches dans le json en entree
-filePatches = open(args.patches)
-patchesData = json.load(filePatches)
-filePatches.close()
-patches = patchesData['features']
+file_patches = open(args.patches)
+patches_data = json.load(file_patches)
+file_patches.close()
+patches = patches_data['features']
 
-listPatches = list()
+list_patches = list()
 for patch in patches:
     if patch['properties']['active'] is True:
         slabs = patch['properties']['slabs']
@@ -87,74 +87,72 @@ for patch in patches:
             x = slab[0]
             y = slab[1]
 
-            slab_path = get_slab_path(int(x), int(y), int(pathDepth))
+            slab_path = get_slab_path(int(x), int(y), int(path_depth))
             tile_path = os.path.join(args.input, 'graph', str(level), slab_path[1:])
-            listPatches.append(os.path.normpath(tile_path+'.tif'))
+            list_patches.append(os.path.normpath(tile_path+'.tif'))
 
 graph_dir = os.path.join(args.input, 'graph', str(level))
 
 # on parcourt le repertoire graph du cache pour recuperer l'ensemble des images de graphe
-listTiles = list()
+list_tiles = list()
 for (root, dirs, files) in os.walk(graph_dir):
     for file in files:
         file = os.path.join(root, file)
-        listTiles.append(os.path.normpath(file))
+        list_tiles.append(os.path.normpath(file))
 
 # fichier intermediaire contenant la liste de images pour le vrt
 path_out = os.path.basename(args.input)
 f_out = open(path_out + '.txt', 'w')
 
-for tile in listTiles:
+for tile in list_tiles:
     # il faut filtrer uniquement les tuiles presente a l'origine
     # on recupere juste le nom de la dalle sans extension -> 2 caracteres
     filename = os.path.basename(tile).split('.')[0]
     if len(filename) > 2:  # cas des tuiles avec retouche
         continue
-    if tile in listPatches:
-        print('Il y a eu retouche')
+    if tile in list_patches:
         # dans ce cas il faut ajouter la tuile index_branche + tilename a la liste
-        #path + index + "_" + basename.tif
-        tilePath = os.path.join(os.path.dirname(tile), str(id_branch)+'_'+os.path.basename(tile))
-        f_out.write(tilePath+'\n')
+        tile_path = os.path.join(os.path.dirname(tile), str(id_branch)+'_'+os.path.basename(tile))
+        f_out.write(tile_path+'\n')
     else:
-        print('Pas de retouche')
         # on ajoute la tuile d'origine dans la liste pour creer le vrt
         f_out.write(tile+'\n')
 
 f_out.close()
 
-tEndPrep = time.perf_counter()
+t_end_prep = time.perf_counter()
 
-tStartVrt1 = time.perf_counter()
+t_start_vrt1 = time.perf_counter()
 
 # on construit un vrt a partir de la liste des images recuperee precedemment
 cmd_buildvrt = (
     "gdalbuildvrt"
     + " -input_file_list "
     + path_out + ".txt "
-    + path_out + "_1.vrt"
+    + path_out + "_tiles.vrt"
 )
 print(cmd_buildvrt)
 os.system(cmd_buildvrt)
 
-tEndVrt1 = time.perf_counter()
+t_end_vrt1 = time.perf_counter()
 
-tStartVrt2 = time.perf_counter()
+t_start_vrt2 = time.perf_counter()
 
-# on construit un 2ème vrt à partir du premier (pour avoir la bonne structure avec les bons paramètres)
+# on construit un 2eme vrt à partir du premier (pour avoir la bonne structure avec les bons parametres :
+# notamment l emprise)
 cmd_buildvrt2 = (
     'gdalbuildvrt '
-    + path_out + '_2.vrt '
-    + path_out + '_1.vrt'
+    + path_out + '_tmp.vrt '
+    + path_out + '_tiles.vrt'
 )
 print(cmd_buildvrt2)
 os.system(cmd_buildvrt2)
 
-tEndVrt2 = time.perf_counter()
+t_end_vrt2 = time.perf_counter()
 
-with open(path_out + '_2.vrt', 'r') as f:
+with open(path_out + '_tmp.vrt', 'r') as f:
     lines = f.readlines()
-with open(path_out + '.vrt', 'w') as f:
+with open(path_out + '_32bits.vrt', 'w') as f:
     for line in lines:
         # on ecrit le code python au bon endroit dans le VRT
         if 'band="1"' in line:
@@ -169,11 +167,11 @@ with open(path_out + '.vrt', 'w') as f:
             f.write(']]>\n')
             f.write('\t</PixelFunctionCode>\n')
         elif 'band="2"' in line:
-            print('Skipping line '+line)
+            print('Skipping line '+line.strip('\n'))
         elif 'band="3"' in line:
-            print('Skipping line '+line)
+            print('Skipping line '+line.strip('\n'))
         elif '</VRTRaster' in line:
-            print('Skipping line '+line)
+            print('Skipping line '+line.strip('\n'))
         elif '<OverviewList' in line:
             f.write('\t</VRTRasterBand>\n')
             f.write(line)
@@ -184,35 +182,32 @@ script = "gdal_polygonize.py"
 if platform.system() == "Windows":
     script = script.split('.')[0]+".bat"
 
-tStartPolygonise = time.perf_counter()
+t_start_polygonise = time.perf_counter()
 
 # on vectorise le graphe à partir du vrt
 cmd_polygonize = (
     script + ' '
-    + os.path.relpath(path_out + '.vrt') + ' '
+    + path_out + '_32bits.vrt '
     + path_out + '.geojson'
-    + ' -f "Geojson"'
+    + ' -f "Geojson" '
+    + '-mask ' + path_out + '_32bits.vrt'
 )
 print(cmd_polygonize)
 os.system(cmd_polygonize)
 
-tEndPolygonise = time.perf_counter()
+t_end_polygonise = time.perf_counter()
 
 print('Nettoyage des fichiers temporaires...')
 if os.exists(path_out + '.txt'):
     os.remove(path_out + '.txt')
-if os.exists(path_out + '_1.vrt'):
-    os.remove(path_out + '_1.vrt')
-if os.exists(path_out + '_2.vrt'):
-    os.remove(path_out + '_2.vrt')
 
 print('Fin\n')
 
-tEnd = time.perf_counter()
+t_end = time.perf_counter()
 
 #temps de calcul des differentes etapes
-print('Temps de preparation du calcul :'+str(tEndPrep-tStartPrep))
-print('Temps de calcul VRT1 :'+str(tEndVrt1-tStartVrt1))
-print('Temps de calcul VRT2 :'+str(tEndVrt2-tStartVrt2))
-print('Temps de calcul polygonise :'+str(tEndPolygonise-tStartPolygonise))
-print('Temps global du calcul :'+str(tEnd-tStart))
+print('Temps de preparation du calcul :'+str(t_end_prep-t_start_prep))
+print('Temps de calcul VRT1 :'+str(t_end_vrt1-t_start_vrt1))
+print('Temps de calcul VRT2 :'+str(t_end_vrt2-t_start_vrt2))
+print('Temps de calcul polygonise :'+str(t_end_polygonise-t_start_polygonise))
+print('Temps global du calcul :'+str(t_end-t_start))
