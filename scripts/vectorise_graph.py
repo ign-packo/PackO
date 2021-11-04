a6ea5a1f81aac749355cfd98f693161835716b0a
Nathan Lenglet <nathan.lenglet@ign.fr>
2021-11-04 14:52:20 +0100

    feat: use tiling for vector export
    feat: add merge/dissolve/make_valid steps to vector export
    
    fix: default value in help menu of vectorise_graph
    
    feat: change tiling method for vector export
    
    fix: keep attribute DN when dissolve
    
    fix: manage polygon/multipolygon mix
    
    refactor: clean code
    
    fix: ogrmerge for unix
    
    refactor: update output logs

--- scripts/vectorise_graph.py
+++ scripts/vectorise_graph.py
@@ -7,7 +7,9 @@ import argparse
 import json
 import platform
 import time
+import shutil
 from cache_def import get_slab_path
+from osgeo import gdal
 
 def read_args():
     """Gestion des arguments"""
@@ -23,6 +25,10 @@ def read_args():
     )
     parser.add_argument(
         "-p", "--patches", required=True, help="file containing patches on the branch to export"
+    )
+    parser.add_argument(
+        "-t", "--tilesize", help="tile size (in pixels) for vectorising graph tiles (default: 100000)", type=int,
+        default=100000
     )
     parser.add_argument(
         "-v", "--verbose", help="verbose (default: 0)", type=int, default=0
@@ -53,7 +59,6 @@ except ValueError:
     args.input = os.path.abspath(args.input)
 print("Updated input path relative to working dir: '" + args.input + "'")
 
-
 # check if input dir exists
 if not os.path.exists(args.input):
     raise SystemExit("Directory " + args.input + " does not exist.")
@@ -69,6 +74,10 @@ fileOverviews.close()
 
 path_depth = overviews['pathDepth']
 level = overviews['level']['max']
+resol = overviews['resolution']
+
+cache_name = os.path.basename((os.path.normpath(args.input)))
+print('Cache name = ' + cache_name)
 
 # valeur par defaut pour verifier l'existance de la branche desiree apres recherche
 id_branch = args.branch
@@ -126,12 +135,16 @@ t_start_vrt1 = time.perf_counter()
 
 # on construit un vrt a partir de la liste des images recuperee precedemment
 cmd_buildvrt = (
-    "gdalbuildvrt"
-    + " -input_file_list "
-    + path_out + ".txt "
-    + path_out + "_tiles.vrt"
+    'gdalbuildvrt'
+    + ' -input_file_list '
+    + path_out + '.txt '
+    + path_out + '_tiles.vrt'
+    + ' -tap'
+    + ' -tr ' + str(resol) + ' ' + str(resol) + ' '
 )
-print(cmd_buildvrt)
+print('Gdalbuildvrt en cours...')
+if args.verbose > 0:
+    print(cmd_buildvrt)
 os.system(cmd_buildvrt)
 
 t_end_vrt1 = time.perf_counter()
@@ -145,7 +158,9 @@ cmd_buildvrt2 = (
     + path_out + '_tmp.vrt '
     + path_out + '_tiles.vrt'
 )
-print(cmd_buildvrt2)
+print('Gdalbuildvrt en cours...')
+if args.verbose > 0:
+    print(cmd_buildvrt2)
 os.system(cmd_buildvrt2)
 
 t_end_vrt2 = time.perf_counter()
@@ -162,21 +177,78 @@ with open(path_out + '_32bits.vrt', 'w') as f:
             f.write('\t<PixelFunctionCode>\n')
             f.write('<![CDATA[\n')
             f.write('import numpy as np\n')
-            f.write('def color_to_int32(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, buf_radius, gt, **kwargs):\n')
+            f.write('def color_to_int32(in_ar, out_ar, xoff, yoff, xsize, ysize, \
+                    raster_xsize, raster_ysize, buf_radius, gt, **kwargs):\n')
             f.write('\tout_ar[:] = in_ar[0] + 256 * in_ar[1] + 256 * 256 * in_ar[2]\n')
             f.write(']]>\n')
             f.write('\t</PixelFunctionCode>\n')
         elif 'band="2"' in line:
-            print('Skipping line '+line.strip('\n'))
+            pass
         elif 'band="3"' in line:
-            print('Skipping line '+line.strip('\n'))
+            pass
         elif '</VRTRaster' in line:
-            print('Skipping line '+line.strip('\n'))
+            pass
         elif '<OverviewList' in line:
             f.write('\t</VRTRasterBand>\n')
             f.write(line)
         else:
             f.write(line)
+
+tmp_dir = os.path.join(args.output, 'tmp')
+if not os.path.exists(tmp_dir):
+    os.mkdir(tmp_dir)
+
+t_start_tiling = time.perf_counter()
+
+# on recupere l'emprise globale du chantier dont on veut extraire xmin, xmax, ymin, ymax
+info = gdal.Info(path_out + '_32bits.vrt')
+infoList = info.split('\n')
+
+ul, lr = '', ''
+for line in infoList:
+    if 'Upper Left' in line:
+        ul = line
+    elif 'Lower Right' in line:
+        lr = line
+
+ul = ul.replace('(', '').replace(')', '').replace(',', '')
+ul_split = ul.split(' ')
+x_min = ul_split[5]
+y_max = ul_split[6]
+
+lr = lr.replace('(', '').replace(')', '').replace(',', '')
+lr_split = lr.split(' ')
+x_max = lr_split[4]
+y_min = lr_split[5]
+
+# ces valeurs vont servir a gerer l'ensemble des gdalbuildvrt sur le chantier
+x_min = int(float(x_min) // 1000 * 1000)
+y_min = int(float(y_min) // 1000 * 1000)
+
+x_max = int((float(x_max) // 1000 + 1) * 1000)
+y_max = int((float(y_max) // 1000 + 1) * 1000)
+
+tile_size = int(resol * args.tilesize)
+
+for x in range(x_min, x_max, tile_size):
+    for y in range(y_min, y_max, tile_size):
+        file = str(x) + '_' + str(y) + '.vrt'
+        cmd_vrt = (
+            'gdalbuildvrt '
+            + os.path.join(tmp_dir, file) + ' '
+            + path_out + '_32bits.vrt'
+            + ' -tr ' + str(resol) + ' ' + str(resol)
+            + ' -te ' + str(x) + ' ' + str(y) + ' ' + str(x+tile_size) + ' ' + str(y+tile_size)
+        )
+        print('Gdalbuildvrt en cours...')
+        if args.verbose > 0:
+            print(cmd_vrt)
+        os.system(cmd_vrt)
+
+t_end_tiling = time.perf_counter()
+
+# on recupere la liste des tuiles creees
+list_tiles_graph = os.listdir(tmp_dir)
 
 script = "gdal_polygonize.py"
 if platform.system() == "Windows":
@@ -184,30 +256,104 @@ if platform.system() == "Windows":
 
 t_start_polygonise = time.perf_counter()
 
-# on vectorise le graphe à partir du vrt
-cmd_polygonize = (
-    script + ' '
-    + path_out + '_32bits.vrt '
-    + path_out + '.geojson'
-    + ' -f "Geojson" '
-    + '-mask ' + path_out + '_32bits.vrt'
-)
-print(cmd_polygonize)
-os.system(cmd_polygonize)
+# on vectorise chaque tuile separement
+for tile in list_tiles_graph:
+    tile_gpkg = tile.split('.')[0] + '.gpkg'
+    gpkg_path = os.path.join(tmp_dir, tile_gpkg)
+    if os.path.exists(gpkg_path):
+        os.remove(gpkg_path)
+    cmd_polygonize = (
+            script + ' '
+            + os.path.join(tmp_dir, tile) + ' '
+            + gpkg_path
+            + ' -f "GPKG" '
+            + '-mask ' + os.path.join(tmp_dir, tile)
+    )
+    print('Gdal_polygonize en cours...')
+    if args.verbose > 0:
+        print(cmd_polygonize)
+    os.system(cmd_polygonize)
 
 t_end_polygonise = time.perf_counter()
 
+script_merge = "ogrmerge.py"
+if platform.system() == "Windows":
+    script = script.split('.')[0]+".bat"
+
+t_start_merge = time.perf_counter()
+
+merge_file = cache_name + '_merge.gpkg'
+merge_path = os.path.join(tmp_dir, merge_file)
+all_gpkg = os.path.join(tmp_dir, '*.gpkg')
+cmd_merge = (
+    script_merge
+    + ' -o ' + merge_path
+    + ' ' + all_gpkg
+    + ' -single'
+    + ' -field_strategy Union'
+)
+print('Ogrmerge en cours...')
+if args.verbose > 0:
+    print(cmd_merge)
+os.system(cmd_merge)
+
+t_end_merge = time.perf_counter()
+
+t_start_dissolve = time.perf_counter()
+
+dissolve_file = cache_name + '_dissolve.gpkg'
+dissolve_path = os.path.join(tmp_dir, dissolve_file)
+cmd_dissolve = (
+    'ogr2ogr '
+    + dissolve_path + ' '
+    + merge_path
+    + ' -nlt PROMOTE_TO_MULTI'
+    + ' -dialect sqlite'
+    + ' -sql "SELECT DN, ST_union(geom) as geom FROM merged GROUP BY DN"'
+)
+print('Ogr2ogr - dissolve en cours...')
+if args.verbose > 0:
+    print(cmd_dissolve)
+os.system(cmd_dissolve)
+
+t_end_dissolve = time.perf_counter()
+
+t_start_make_valid = time.perf_counter()
+
+valid_file = cache_name + '.gpkg'
+valid_path = os.path.join(args.output, valid_file)
+cmd_make_valid = (
+    'ogr2ogr '
+    + valid_path + ' '
+    + dissolve_path
+    + ' -nlt PROMOTE_TO_MULTI'
+    + ' -makevalid'
+)
+print('Ogr2ogr - make_valid en cours...')
+if args.verbose > 0:
+    print(cmd_make_valid)
+os.system(cmd_make_valid)
+
+t_end_make_valid = time.perf_counter()
+
 print('Nettoyage des fichiers temporaires...')
-if os.exists(path_out + '.txt'):
+if os.path.exists(path_out + '.txt'):
     os.remove(path_out + '.txt')
+if os.path.exists(tmp_dir) and os.path.exists(valid_path):
+    shutil.rmtree(tmp_dir)
 
 print('Fin\n')
 
 t_end = time.perf_counter()
 
-#temps de calcul des differentes etapes
-print('Temps de preparation du calcul :'+str(t_end_prep-t_start_prep))
-print('Temps de calcul VRT1 :'+str(t_end_vrt1-t_start_vrt1))
-print('Temps de calcul VRT2 :'+str(t_end_vrt2-t_start_vrt2))
-print('Temps de calcul polygonise :'+str(t_end_polygonise-t_start_polygonise))
-print('Temps global du calcul :'+str(t_end-t_start))
+# temps de calcul des differentes etapes
+if args.verbose > 0:
+    print('Temps de preparation du calcul :'+str(round(t_end_prep-t_start_prep, 2)))
+    print('Temps de calcul VRT1 :'+str(round(t_end_vrt1-t_start_vrt1, 2)))
+    print('Temps de calcul VRT2 :'+str(round(t_end_vrt2-t_start_vrt2, 2)))
+    print('Temps de calcul tiling :'+str(round(t_end_tiling-t_start_tiling, 2)))
+    print('Temps de calcul polygonise :'+str(round(t_end_polygonise-t_start_polygonise, 2)))
+    print('Temps de calcul merge :'+str(round(t_end_merge-t_start_merge, 2)))
+    print('Temps de calcul dissolve :'+str(round(t_end_dissolve-t_start_dissolve, 2)))
+    print('Temps de calcul make_valid :'+str(round(t_end_make_valid-t_start_make_valid, 2)))
+    print('Temps global du calcul :'+str(round(t_end-t_start, 2)))
