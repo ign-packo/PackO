77e15611d029bf5599f0b47816f62e9f0f256f44
ftoromanoff <francois.toromanoff@ign.fr>
2022-07-13 17:51:14 +0200

    ci(API): rewrite test regress -> 3_graph.js

--- regress/3_graph.js
+++ regress/3_graph.js
@@ -6,101 +6,26 @@ const should = chai.should();
 const fs = require('fs');
 const app = require('..');
 
-const testOpiRgb = [
+const testOpi = [
   { name: '19FD5606Ax00020_16371', date: '2019-07-04', time: '13:33:00' },
   { name: '19FD5606Ax00020_16372', date: '2019-07-04', time: '13:33:00' },
   { name: '19FD5606Ax00020_16373', date: '2019-07-04', time: '13:33:00' },
 ];
 
-const testOpiIr = [
-  { name: '19FD5606A_ix00020_16371', date: '2019-07-04', time: '13:33:00' },
-  { name: '19FD5606A_ix00020_16372', date: '2019-07-04', time: '13:33:00' },
-  { name: '19FD5606A_ix00020_16373', date: '2019-07-04', time: '13:33:00' },
-];
+const cachePath = './cache_test/cache_test_RGBIR';
+const overviews = JSON.parse(fs.readFileSync(`${cachePath}/overviews.json`, 'utf8'));
+const cacheName = 'cacheRegress';
 
-const params = [
-  // Les caches générés par l'intégration continue
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_regress_RGB/overviews.json', 'utf8')),
-    cacheName: 'cacheRegressRgb',
-    cachePath: 'cache_regress_RGB',
-    idCache: null,
-    idBranch: {},
-    testOpi: testOpiRgb,
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_regress_RGBIR/overviews.json', 'utf8')),
-    cacheName: 'cacheRegressRgbir',
-    cachePath: 'cache_regress_RGBIR',
-    idCache: null,
-    idBranch: {},
-    testOpi: testOpiRgb,
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_regress_IR/overviews.json', 'utf8')),
-    cacheName: 'cacheRegressIr',
-    cachePath: 'cache_regress_IR',
-    idCache: null,
-    idBranch: {},
-    testOpi: testOpiIr,
-  },
-  // Les caches présents dans le dépôt
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_test/cache_test_RGB/overviews.json', 'utf8')),
-    cacheName: 'cacheTestRgb',
-    cachePath: 'cache_test/cache_test_RGB/',
-    idCache: null,
-    idBranch: {},
-    testOpi: testOpiRgb,
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_test/cache_test_RGBIR/overviews.json', 'utf8')),
-    cacheName: 'cacheTestRgbir',
-    cachePath: 'cache_test/cache_test_RGBIR/',
-    idCache: null,
-    idBranch: {},
-    testOpi: testOpiRgb,
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_test/cache_test_IR/overviews.json', 'utf8')),
-    cacheName: 'cacheTestIr',
-    cachePath: 'cache_test/cache_test_IR/',
-    idCache: null,
-    idBranch: {},
-    testOpi: testOpiIr,
-  },
-];
-
-function setIdCache(cacheName, idCache) {
-  params.forEach((param) => {
-    if (param.cacheName === cacheName) {
-      /* eslint-disable no-param-reassign */
-      param.idCache = idCache;
-      /* eslint-enable no-param-reassign */
-    }
-  });
+let idCache = null;
+function setIdCache(id) {
+  idCache = id;
 }
-
-function setIdBranch(cacheName, branchName, idBranch) {
-  params.forEach((param) => {
-    if (param.cacheName === cacheName) {
-      /* eslint-disable no-param-reassign */
-      param.idBranch[branchName] = idBranch;
-      /* eslint-enable no-param-reassign */
-    }
-  });
-}
-
-// let idCache = null;
-// function setIdCache(id) {
-//   idCache = id;
-// }
 
 const branchName = 'graphRegress';
-// let idBranch = null;
-// function setIdBranch(id) {
-//   idBranch = id;
-// }
+const idBranch = {};
+function setIdBranch(name, id) {
+  idBranch[name] = id;
+}
 
 const schema = {
   title: 'test',
@@ -121,184 +46,181 @@ const schema = {
   },
 };
 
-describe('Graph', () => {
+describe('route/graph.js', () => {
   after((done) => {
     app.server.close();
     done();
   });
 
-  params.forEach((param) => {
-    describe('create a test cache', () => {
-      it(`should return a cacheId on ${param.cacheName}`, (done) => {
+  describe('create a test cache', () => {
+    it('should return a cacheId', (done) => {
+      chai.request(app)
+        .post('/cache')
+        .query({
+          name: cacheName,
+          path: cachePath,
+        })
+        .send(overviews)
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(200);
+          const resJson = JSON.parse(res.text);
+          resJson.should.have.property('id_cache');
+          setIdCache(resJson.id_cache);
+          resJson.should.have.property('name').equal(cacheName);
+          done();
+        });
+    });
+
+    describe('create a test branch', () => {
+      it('should return a branchId', (done) => {
         chai.request(app)
-          .post('/cache')
+          .post('/branch')
           .query({
-            name: param.cacheName,
-            path: param.cachePath,
+            name: branchName,
+            idCache,
           })
-          .send(param.overviews)
           .end((err, res) => {
             should.not.exist(err);
             res.should.have.status(200);
             const resJson = JSON.parse(res.text);
-            resJson.should.have.property('id_cache');
-            setIdCache(param.cacheName, resJson.id_cache);
-            resJson.should.have.property('name').equal(param.cacheName);
+            resJson.should.have.property('id');
+            setIdBranch(branchName, resJson.id);
+            resJson.should.have.property('name').equal(branchName);
             done();
           });
       });
+    });
 
-      describe(`create a test branch on ${param.cacheName}`, () => {
-        it('should return a branchId', (done) => {
+    describe('GET /{idBranch}/graph', () => {
+      describe('query: x=0 & y=0', () => {
+        it("should return a 'out of bounds'", (done) => {
           chai.request(app)
-            .post('/branch')
-            .query({
-              name: branchName,
-              idCache: param.idCache,
-            })
+            .get(`/${idBranch[branchName]}/graph`)
+            .query({ x: 0, y: 0 })
+            .end((err, res) => {
+              should.not.exist(err);
+              res.should.have.status(244);
+              const resJson = JSON.parse(res.text);
+              resJson.should.have.property('msg').equal('out of bounds');
+
+              done();
+            });
+        });
+      });
+      describe('query: x=230757 & y=6759654', () => {
+        // outside of graph but inside the images frame
+        it("should return a 'out of graph'", (done) => {
+          chai.request(app)
+            .get(`/${idBranch[branchName]}/graph`)
+            .query({ x: 230757, y: 6759654 })
+            .end((err, res) => {
+              should.not.exist(err);
+              res.should.have.status(244);
+              const resJson = JSON.parse(res.text);
+              resJson.should.have.property('msg').equal('out of graph');
+              done();
+            });
+        });
+      });
+      describe('query: x=230755 & y=6759650', () => {
+        it(`should return a Json { "color": Array(3), "opiName": ${testOpi[0].name}, "date": ${testOpi[0].date}, "time": ${testOpi[0].time} }`, (done) => {
+          chai.request(app)
+            .get(`/${idBranch[branchName]}/graph`)
+            .query({ x: 230755, y: 6759650 })
             .end((err, res) => {
               should.not.exist(err);
               res.should.have.status(200);
               const resJson = JSON.parse(res.text);
-              resJson.should.have.property('id');
-              setIdBranch(param.cacheName, branchName, resJson.id);
-              resJson.should.have.property('name').equal(branchName);
+              resJson.should.be.jsonSchema(schema);
+              resJson.should.have.property('opiName').equal(testOpi[0].name);
+              resJson.should.have.property('date').equal(testOpi[0].date);
+              resJson.should.have.property('time').equal(testOpi[0].time);
+              done();
+            });
+        });
+      });
+      describe('query: x=230749.8 & y=6759645.1', () => {
+        it(`should return a Json { "color": Array(3), "opiName": ${testOpi[1].name}, "date": ${testOpi[1].date}, "time": ${testOpi[1].time} }`, (done) => {
+          chai.request(app)
+            .get(`/${idBranch[branchName]}/graph`)
+            .query({ x: 230749.8, y: 6759645.1 })
+            .end((err, res) => {
+              should.not.exist(err);
+              res.should.have.status(200);
+              const resJson = JSON.parse(res.text);
+              resJson.should.be.jsonSchema(schema);
+              resJson.should.have.property('opiName').equal(testOpi[1].name);
+              resJson.should.have.property('date').equal(testOpi[1].date);
+              resJson.should.have.property('time').equal(testOpi[1].time);
+              done();
+            });
+        });
+      });
+      describe('query: x=230747.7 & y=6759644.', () => {
+        it(`should return a Json { "color": Array(3), "opiName": ${testOpi[2].name}, "date": ${testOpi[2].date}, "time": ${testOpi[2].time} }`, (done) => {
+          chai.request(app)
+            .get(`/${idBranch[branchName]}/graph`)
+            .query({ x: 230747.7, y: 6759644 })
+            .end((err, res) => {
+              should.not.exist(err);
+              res.should.have.status(200);
+              const resJson = JSON.parse(res.text);
+              resJson.should.be.jsonSchema(schema);
+              resJson.should.have.property('opiName').equal(testOpi[2].name);
+              resJson.should.have.property('date').equal(testOpi[2].date);
+              resJson.should.have.property('time').equal(testOpi[2].time);
               done();
             });
         });
       });
 
-      describe(`GET /{idBranch}/graph on ${param.cacheName}`, () => {
-        describe('query: x=0 & y=0', () => {
-          it("should return a 'out of bounds'", (done) => {
-            chai.request(app)
-              .get(`/${param.idBranch[branchName]}/graph`)
-              .query({ x: 0, y: 0 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(244);
-                const resJson = JSON.parse(res.text);
-                resJson.should.have.property('msg').equal('out of bounds');
-
-                done();
-              });
-          });
-        });
-        describe('query: x=230757 & y=6759654', () => {
-          // outside of graph but inside the images frame
-          it("should return a 'out of graph'", (done) => {
-            chai.request(app)
-              .get(`/${param.idBranch[branchName]}/graph`)
-              .query({ x: 230757, y: 6759654 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(244);
-                const resJson = JSON.parse(res.text);
-                resJson.should.have.property('msg').equal('out of graph');
-                done();
-              });
-          });
-        });
-        describe('query: x=230755 & y=6759650', () => {
-          it(`should return a Json { "color": Array(3), "opiName": ${param.testOpi[0].name}, "date": ${param.testOpi[0].date}, "time": ${param.testOpi[0].time} }`, (done) => {
-            chai.request(app)
-              .get(`/${param.idBranch[branchName]}/graph`)
-              .query({ x: 230755, y: 6759650 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(200);
-                const resJson = JSON.parse(res.text);
-                resJson.should.be.jsonSchema(schema);
-                resJson.should.have.property('opiName').equal(param.testOpi[0].name);
-                resJson.should.have.property('date').equal(param.testOpi[0].date);
-                resJson.should.have.property('time').equal(param.testOpi[0].time);
-                done();
-              });
-          });
-        });
-        describe('query: x=230749.8 & y=6759645.1', () => {
-          it(`should return a Json { "color": Array(3), "opiName": ${param.testOpi[1].name}, "date": ${param.testOpi[1].date}, "time": ${param.testOpi[1].time} }`, (done) => {
-            chai.request(app)
-              .get(`/${param.idBranch[branchName]}/graph`)
-              .query({ x: 230749.8, y: 6759645.1 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(200);
-                const resJson = JSON.parse(res.text);
-                resJson.should.be.jsonSchema(schema);
-                resJson.should.have.property('opiName').equal(param.testOpi[1].name);
-                resJson.should.have.property('date').equal(param.testOpi[1].date);
-                resJson.should.have.property('time').equal(param.testOpi[1].time);
-                done();
-              });
-          });
-        });
-        describe('query: x=230747.7 & y=6759644.', () => {
-          it(`should return a Json { "color": Array(3), "opiName": ${param.testOpi[2].name}, "date": ${param.testOpi[2].date}, "time": ${param.testOpi[2].time} }`, (done) => {
-            chai.request(app)
-              .get(`/${param.idBranch[branchName]}/graph`)
-              .query({ x: 230747.7, y: 6759644 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(200);
-                const resJson = JSON.parse(res.text);
-                resJson.should.be.jsonSchema(schema);
-                resJson.should.have.property('opiName').equal(param.testOpi[2].name);
-                resJson.should.have.property('date').equal(param.testOpi[2].date);
-                resJson.should.have.property('time').equal(param.testOpi[2].time);
-                done();
-              });
-          });
-        });
-
-        describe('query: x=230748 & y=6759643', () => {
-          // image not yet in the cache
-          it("should return a 'out of graph'", (done) => {
-            chai.request(app)
-              .get(`/${param.idBranch[branchName]}/graph`)
-              .query({ x: 230748, y: 6759643 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(244);
-                const resJson = JSON.parse(res.text);
-                resJson.should.have.property('msg').equal('out of graph');
-
-                done();
-              });
-          });
-        });
-        describe('query: x=230747 & y=6759643', () => {
-          // branch doesn't exist
-          it("idBranch=99999 => should return a 'branch does not exist'", (done) => {
-            chai.request(app)
-              .get('/99999/graph')
-              .query({ x: 230747, y: 6759643 })
-              .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(400);
-                const resJson = JSON.parse(res.text);
-                resJson.should.be.an('array').to.have.lengthOf(1);
-                resJson[0].should.have.property('status').equal("Le paramètre 'idBranch' n'est pas valide.");
-                done();
-              });
-          });
-        });
-      });
-
-      describe('delete the test cache', () => {
-        it('should succeed', (done) => {
+      describe('query: x=230748 & y=6759643', () => {
+        // image not yet in the cache
+        it("should return a 'out of graph'", (done) => {
           chai.request(app)
-            .delete('/cache')
-            .query({ idCache: param.idCache })
+            .get(`/${idBranch[branchName]}/graph`)
+            .query({ x: 230748, y: 6759643 })
             .end((err, res) => {
               should.not.exist(err);
-              res.should.have.status(200);
+              res.should.have.status(244);
               const resJson = JSON.parse(res.text);
-              resJson.should.equal(`cache '${param.cacheName}' détruit`);
+              resJson.should.have.property('msg').equal('out of graph');
+              done();
+            });
+        });
+      });
+      describe('query: x=230747 & y=6759643', () => {
+        // branch doesn't exist
+        it("idBranch=99999 => should return a 'branch does not exist'", (done) => {
+          chai.request(app)
+            .get('/99999/graph')
+            .query({ x: 230747, y: 6759643 })
+            .end((err, res) => {
+              should.not.exist(err);
+              res.should.have.status(400);
+              const resJson = JSON.parse(res.text);
+              resJson.should.be.an('array').to.have.lengthOf(1);
+              resJson[0].should.have.property('status').equal("Le paramètre 'idBranch' n'est pas valide.");
               done();
             });
         });
       });
     });
-  });// params.forEach
+
+    describe('delete the test cache', () => {
+      it('should succeed', (done) => {
+        chai.request(app)
+          .delete('/cache')
+          .query({ idCache })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            const resJson = JSON.parse(res.text);
+            resJson.should.equal(`cache '${cacheName}' détruit`);
+            done();
+          });
+      });
+    });
+  });
 });
