72ac757dcca96bf2d48db04eff5a3a227c8e72fc
ftoromanoff <francois.toromanoff@ign.fr>
2022-07-21 12:07:45 +0200

    ci(API): rewrite test regress -> 2_cache.js

--- regress/2_cache.js
+++ regress/2_cache.js
@@ -7,52 +7,17 @@ const fs = require('fs');
 
 const app = require('..');
 
-const params = [
-// Les caches générés par l'intégration continue
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_regress_RGB/overviews.json', 'utf8')),
-    cacheName: 'cacheRegressRgb',
-    cachePath: 'cache_regress_RGB',
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_regress_RGBIR/overviews.json', 'utf8')),
-    cacheName: 'cacheRegressRgbir',
-    cachePath: 'cache_regress_RGBIR',
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_regress_IR/overviews.json', 'utf8')),
-    cacheName: 'cacheRegressIr',
-    cachePath: 'cache_regress_IR',
-  },
-  // Les caches présents dans le dépôt
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_test/cache_test_RGB/overviews.json', 'utf8')),
-    cacheName: 'cacheTestRgb',
-    cachePath: 'cache_test/cache_test_RGB/',
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_test/cache_test_RGBIR/overviews.json', 'utf8')),
-    cacheName: 'cacheTestRgbir',
-    cachePath: 'cache_test/cache_test_RGBIR/',
-  },
-  {
-    overviews: JSON.parse(fs.readFileSync('./cache_test/cache_test_IR/overviews.json', 'utf8')),
-    cacheName: 'cacheTestIr',
-    cachePath: 'cache_test/cache_test_IR/',
-  },
-];
+// const cachePath = '/cache_regress';
+const cachePath = './cache_test/cache_test_RGBIR';
+const overviews = JSON.parse(fs.readFileSync(`${cachePath}/overviews.json`, 'utf8'));
+const cacheName = 'cacheRegress';
 
-function setIdCache(cacheName, idCache) {
-  params.forEach((param) => {
-    if (param.cacheName === cacheName) {
-      /* eslint-disable no-param-reassign */
-      param.idCache = idCache;
-      /* eslint-enable no-param-reassign */
-    }
-  });
+let idCache = null;
+function setIdCache(id) {
+  idCache = id;
 }
 
-describe('Cache', () => {
+describe('route/cache.js', () => {
   after((done) => {
     app.server.close();
     done();
@@ -75,87 +40,99 @@ describe('Cache', () => {
   });
 
   describe('POST /cache', () => {
-    params.forEach((param) => {
-      describe(`insert an overviews.json on ${param.cacheName}`, () => {
-        it('should succeed', (done) => {
-          chai.request(app)
-            .post('/cache')
-            .query({
-              name: param.cacheName,
-              path: param.cachePath,
-            })
-            .send(param.overviews)
-            .end((err, res) => {
-              should.not.exist(err);
-              res.should.have.status(200);
-              const resJson = JSON.parse(res.text);
-              resJson.should.have.property('id_cache');
-              // setIdCache(resJson.id_cache);
-              // setCacheName(param.cacheName)
-              setIdCache(param.cacheName, resJson.id_cache);
-              resJson.should.have.property('name').equal(param.cacheName);
-              done();
-            });
-        });
+    describe('insert a cache with no name', () => {
+      // no change in test coverage but it's checked in the code
+      it('should return an error', (done) => {
+        chai.request(app)
+          .post('/cache')
+          .query({
+            path: cachePath,
+          })
+          .send(overviews)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(400);
+            const resJson = JSON.parse(res.text);
+            resJson.should.be.an('array').to.have.lengthOf(1);
+            resJson[0].should.have.property('status').equal("Le paramètre 'name' est requis.");
+            done();
+          });
       });
-      describe(`insert a cache already in the base on ${param.cacheName}`, () => {
-        it('should return an error', (done) => {
-          chai.request(app)
-            .post('/cache')
-            .query({
-              name: param.cacheName,
-              path: param.cachePath,
-            })
-            .send(param.overviews)
-            .end((err, res) => {
-              should.not.exist(err);
-              res.should.have.status(406);
-              const resJson = JSON.parse(res.text);
-              resJson.should.be.an('object');
-              resJson.should.have.property('msg').equal('A cache with this name already exists.');
-              done();
-            });
-        });
+    });
+    describe('insert a cache with a valid overviews.json', () => {
+      it('should succeed', (done) => {
+        chai.request(app)
+          .post('/cache')
+          .query({
+            name: cacheName,
+            path: cachePath,
+          })
+          .send(overviews)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            const resJson = JSON.parse(res.text);
+            resJson.should.have.property('id_cache');
+            setIdCache(resJson.id_cache);
+            resJson.should.have.property('name').equal(cacheName);
+            done();
+          });
       });
-      describe(`insert an overviews.json on ${param.cacheName}`, () => {
-        it(' list_OPI = [] => should return an error', (done) => {
-          const paramTest = param;
-          delete paramTest.overviews.list_OPI;
-          chai.request(app)
-            .post('/cache')
-            .query({
-              name: paramTest.cacheName,
-              path: paramTest.cachePath,
-            })
-            .send(paramTest.overviews)
-            .end((err, res) => {
-              should.not.exist(err);
-              res.should.have.status(400);
-              const resJson = JSON.parse(res.text);
-              resJson.should.be.an('array').to.have.lengthOf(1);
-              resJson[0].should.have.property('status').equal("Le paramètre 'list_OPI' est requis.");
-              done();
-            });
-        });
+    });
+    describe('insert a cache already in the base', () => {
+      it('should return an error', (done) => {
+        chai.request(app)
+          .post('/cache')
+          .query({
+            name: cacheName,
+            path: cachePath,
+          })
+          .send(overviews)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(406);
+            const resJson = JSON.parse(res.text);
+            resJson.should.be.an('object');
+            resJson.should.have.property('msg').equal('A cache with this name already exists.');
+            done();
+          });
+      });
+    });
+    describe('insert a cache with an overviews.json with no key list_OPI', () => {
+      it('should return an error', (done) => {
+        delete overviews.list_OPI;
+        chai.request(app)
+          .post('/cache')
+          .query({
+            name: cacheName,
+            path: cachePath,
+          })
+          .send(overviews)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(400);
+            const resJson = JSON.parse(res.text);
+            resJson.should.be.an('array').to.have.lengthOf(1);
+            resJson[0].should.have.property('status').equal("Le paramètre 'list_OPI' est requis.");
+            done();
+          });
       });
     });
   });
 
   describe('DELETE /cache', () => {
-    params.forEach((param) => {
-      describe(`delete a valid cache on ${param.cacheName}`, () => {
-        it('should succeed', (done) => {
-          chai.request(app)
-            .delete('/cache')
-            .query({ idCache: param.idCache })
-            .end((err, res) => {
-              should.not.exist(err);
-              res.should.have.status(200);
-              const resJson = JSON.parse(res.text);
-              resJson.should.equal(`cache '${param.cacheName}' détruit`);
-              done();
-            });
-        });
+    describe('delete a valid cache', () => {
+      it('should succeed', (done) => {
+        chai.request(app)
+          .delete('/cache')
+          .query({ idCache })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            const resJson = JSON.parse(res.text);
+            resJson.should.equal(`cache '${cacheName}' détruit`);
+            done();
+          });
       });
     });
     describe('delete a non existing cache', () => {
@@ -172,6 +149,19 @@ describe('Cache', () => {
             done();
           });
       });
+    });
+  });
+});
+
+describe('middlewares/cache.js', () => {
+  describe('getCachePath', () => {
+    it('not tested yet', (done) => {
+      done();
+    });
+  });
+  describe('getOverviews', () => {
+    it('not tested yet', (done) => {
+      done();
     });
   });
 });
