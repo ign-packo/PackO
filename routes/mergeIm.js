43eee66f763d195809e7563f36a33aac29c0844c
Ana-Maria Rosu <ana-maria.rosu@ign.fr>
2024-10-24 15:07:00 +0200

    refactor merge: add type as param

--- routes/mergeIm.js
+++ routes/mergeIm.js
@@ -12,6 +12,8 @@ router.get('/mergeImages', [
     .exists().withMessage(createErrMsg.missingParameter('im1')),
   query('im2')
     .exists().withMessage(createErrMsg.missingParameter('im2')),
+  query('type')
+    .exists().withMessage(createErrMsg.missingParameter('type')),
   query('output')
     .exists().withMessage(createErrMsg.missingParameter('output')),
 ], validateParams, (req, res, next) => {
@@ -22,7 +24,7 @@ router.get('/mergeImages', [
   }
   const params = matchedData(req);
   const {
-    im1, im2, output,
+    im1, im2, type, output,
   } = params;
 
   console.log('Parameters: ', params);
@@ -31,7 +33,12 @@ router.get('/mergeImages', [
   const start = Date.now();
   const ds1 = gdal.open(im1);
   const ds2 = gdal.open(im2);
-  gdal.warp(output, null, [ds1, ds2], ['-of', 'COG', '-co', 'COMPRESS=JPEG', '-co', 'QUALITY=90']);
+  if (type == "opi/ortho") {
+    gdal.warp(output, null, [ds1, ds2], ['-of', 'COG', '-co', 'COMPRESS=JPEG', '-co', 'QUALITY=90']);
+  }
+  else if (type == "graph") {
+    gdal.warp(output, null, [ds1, ds2], ['-of', 'COG', '-co', 'COMPRESS=LZW', '-co', 'RESAMPLING=NEAREST']);
+  }
   ds1.close();
   ds2.close();
   const end = Date.now();
